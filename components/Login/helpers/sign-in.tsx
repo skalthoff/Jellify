47c960c7ea72c2c5ab69183f32b984cce00ae4cc
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-13 16:23:31 -0500

    getting a cool message to display

--- components/Login/helpers/sign-in.tsx
+++ components/Login/helpers/sign-in.tsx
@@ -3,6 +3,6 @@ import { Text } from "react-native";
 export default function SignIn(): React.JSX.Element {
 
     return (
-        <Text></Text>
+        <Text>Alyssa please be impressed</Text>
     )
 }
