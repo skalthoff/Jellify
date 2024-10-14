a1ab766553d3a2c4fa6b94542e0a776dc4280f21
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-14 06:59:17 -0500

    organizing login flow

---
+++ components/Login/helpers/server-address.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { View, TextInput, Button, StyleSheet } from "react-native";
+import { handleServerUrlChangeEvent } from "../utils/handlers";
+import { useServerUrl } from "@/api/queries";
+import { validateServerUrl } from "../utils/validation";
+import { useServerUrl as serverUrlMutation } from "../../../api/mutators/storage";
+
+const styles = StyleSheet.create({
+    input: {
+        height: 40,
+        margin: 12,
+        borderRadius: 1,
+        borderWidth: 1,
+        padding: 10,
+    }
+})
+
+export default function ServerAddress(): React.JSX.Element {
+
+    let [serverUrl, setServerUrl] = useState(useServerUrl().data);
+
+    let serverUrlIsValid = validateServerUrl(serverUrl);
+
+    return (
+        <View>
+
+                <TextInput
+                    style={styles.input}
+                    value={serverUrl}
+                    onChangeText={(value) => handleServerUrlChangeEvent(value, setServerUrl)}
+                    />
+
+                <Button 
+                    onPress={(event) => serverUrlMutation.mutate(serverUrl)}
+                    disabled={!serverUrlIsValid}
+                    title="Connect"/>
+            </View>
+    )
+}
---
+++ components/Login/helpers/server-library.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View } from "react-native";
+
+export default function ServerLibrary(): React.JSX.Element {
+    return (
+        <View>
+            
+        </View>
+    )
+}
