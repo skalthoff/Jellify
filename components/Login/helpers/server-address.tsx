6096c28014a604a1986c16cd04a899ee8e169f01
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-14 12:38:59 -0500

    plz

--- components/Login/helpers/server-address.tsx
+++ components/Login/helpers/server-address.tsx
@@ -12,11 +12,12 @@ export default function ServerAddress(): React.JSX.Element {
         <YStack>
                 <Input placeholder="Jellyfin Server Address"
                     // onChangeText={(value) => validateServerUrl(value) ?? setServerUrl(value)}
-                    />
+                    ></Input>
 
                 <Button 
                     // onPress={(event) => serverUrlMutation.mutate(serverUrl)}
-                    disabled={_.isEmpty(serverUrl)}>
+                    // disabled={_.isEmpty(serverUrl)}
+                    >
                         Connect
                 </Button>
         </YStack>
