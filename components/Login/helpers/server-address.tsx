bc79e395d314ae8b505cc7ca75c13aa2052786b6
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-14 14:00:38 -0500

    what about if we do it this way??

--- components/Login/helpers/server-address.tsx
+++ components/Login/helpers/server-address.tsx
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 import { validateServerUrl } from "../utils/validation";
-import { serverUrlMutation } from "../../../api/mutators/storage";
 import _ from "lodash";
 import { Button, Input, YStack } from "tamagui";
+import { Jellyfin } from "@jellyfin/sdk";
+import { getSystemApi } from "@jellyfin/sdk/lib/utils/api/system-api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useMutation } from "@tanstack/react-query";
+import { client } from "../../../api/queries";
+import { AsyncStorageKeys } from "../../../enums/async-storage-keys";
 
 export default function ServerAddress(): React.JSX.Element {
 
     const [serverUrl, setServerUrl] = useState("");
 
-    const handleServerUrlMutation = (serverUrl: string) => {
-        serverUrlMutation.mutate(serverUrl)
-    }
+    const serverUrlMutation = useMutation({
+        mutationFn: async (serverUrl: string | undefined) => {
+    
+            console.log("Mutating server URL");
+    
+            if (!!!serverUrl)
+                throw Error("Server URL was empty")
+    
+            let jellyfin = new Jellyfin(client);
+            let api = jellyfin.createApi(serverUrl);
+            return await getSystemApi(api).getPublicSystemInfo()
+        },
+        onSuccess: (publicSystemInfoResponse, serverUrl, context) => {
+            if (!!!publicSystemInfoResponse.data.Version)
+                throw new Error("Unable to connect to Jellyfin Server");
+    
+            console.log(`Connected to Jellyfin ${publicSystemInfoResponse.data.Version!}`);
+            return AsyncStorage.setItem(AsyncStorageKeys.ServerUrl, serverUrl!);
+        }
+    });
 
     return (
         <YStack>
@@ -19,8 +41,8 @@ export default function ServerAddress(): React.JSX.Element {
                     ></Input>
 
                 <Button 
-                    onPress={() => handleServerUrlMutation(serverUrl)}
-                    disabled={_.isEmpty(serverUrl)}>
+                    onPress={() => serverUrlMutation.mutate(serverUrl)}
+                    disabled={_.isEmpty(serverUrl) || serverUrlMutation.isPending}>
                         Connect
                 </Button>
         </YStack>
