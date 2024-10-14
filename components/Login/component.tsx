3170519c23f8d61ec2e2199d6cc195f355c13749
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-14 06:59:17 -0500

    organizing login flow

--- components/Login/component.tsx
+++ components/Login/component.tsx
@@ -1,49 +1,22 @@
 import { createStackNavigator } from "@react-navigation/stack";
-import { Button, StyleSheet, TextInput, useColorScheme, View } from "react-native";
-import { useState } from "react";
+import { useColorScheme } from "react-native";
 import { useServerUrl } from "../../api/queries";
-import { useServerUrl as serverUrlMutation } from "../../api/mutators/storage";
 import _ from "lodash"
 import ServerAuthentication from "./helpers/server-authentication";
-import { validateServerUrl } from "./utils/validation";
-import { handleServerUrlChangeEvent } from "./utils/handlers";
+import ServerAddress from "./helpers/server-address";
 
 export default function Login(): React.JSX.Element {
 
     const isDarkMode = useColorScheme() === 'dark';
-    
-    const styles = StyleSheet.create({
-        input: {
-            height: 40,
-            margin: 12,
-            borderRadius: 1,
-            borderWidth: 1,
-            padding: 10,
-            color: isDarkMode ? "white" : "black"
-        }
-    })
 
     const Stack = createStackNavigator();
 
-    let [serverUrl, setServerUrl] = useState(useServerUrl().data);
 
-    let serverUrlIsValid = validateServerUrl(serverUrl);
+    let { isError, isSuccess } = useServerUrl();
 
     return (
-        (_.isEmpty(serverUrl) ?
-            <View>
-
-                <TextInput
-                    style={styles.input}
-                    value={serverUrl}
-                    onChangeText={(value) => handleServerUrlChangeEvent(value, setServerUrl)}
-                    />
-
-                <Button 
-                    onPress={(event) => serverUrlMutation.mutate(serverUrl)}
-                    disabled={!serverUrlIsValid}
-                    title="Connect"/>
-            </View>
+        (isError ?
+            <ServerAddress />
         : 
             <ServerAuthentication />
         )
