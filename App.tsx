81292325eaf93f1a82ff28ca3440d0cfeba3ec4e
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-14 12:42:34 -0500

    OMFG
    Did you RTFM

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import './gesture-handler';
 import React from 'react';
+import defaultConfig from '@tamagui/config/v3'
 
 import Jellify from './components/jellify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createTamagui, TamaguiProvider } from 'tamagui';
 
 export default function App(): React.JSX.Element {
 
   const queryClient = new QueryClient();
 
+  const config = createTamagui(defaultConfig)
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Jellify />
+      <TamaguiProvider config={config}>
+        <Jellify />
+      </TamaguiProvider>
     </QueryClientProvider>
   );
 }
--- components/Login/helpers/server-address.tsx
+++ components/Login/helpers/server-address.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { validateServerUrl } from "../utils/validation";
+import { useServerUrl as serverUrlMutation } from "../../../api/mutators/storage";
 import _ from "lodash";
 import { Button, Input, YStack } from "tamagui";
 
@@ -9,13 +11,12 @@ export default function ServerAddress(): React.JSX.Element {
     return (
         <YStack>
                 <Input placeholder="Jellyfin Server Address"
-                    // onChangeText={(value) => validateServerUrl(value) ?? setServerUrl(value)}
+                    onChangeText={(value) => validateServerUrl(value) ?? setServerUrl(value)}
                     ></Input>
 
                 <Button 
-                    // onPress={(event) => serverUrlMutation.mutate(serverUrl)}
-                    // disabled={_.isEmpty(serverUrl)}
-                    >
+                    onPress={(event) => serverUrlMutation.mutate(serverUrl)}
+                    disabled={_.isEmpty(serverUrl)}>
                         Connect
                 </Button>
         </YStack>
