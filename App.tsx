0332ae7d84887be7d6bc0d581de569783fd687bc
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-15 10:13:29 -0500

    using wix ui library instead

--- App.tsx
+++ App.tsx
@@ -1,23 +1,18 @@
 import './gesture-handler';
 import React from 'react';
 import "react-native-url-polyfill/auto";
-import defaultConfig from '@tamagui/config/v3'
 
 import Jellify from './components/jellify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { createTamagui, TamaguiProvider } from 'tamagui';
 
 export default function App(): React.JSX.Element {
 
   const queryClient = new QueryClient();
 
-  const config = createTamagui(defaultConfig)
 
   return (
     <QueryClientProvider client={queryClient}>
-      <TamaguiProvider config={config}>
           <Jellify />
-      </TamaguiProvider>
     </QueryClientProvider>
   );
 }
--- components/Login/helpers/server-address.tsx
+++ components/Login/helpers/server-address.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { validateServerUrl } from "../utils/validation";
 import _ from "lodash";
-import { Button, Input, SizableText, YStack } from "tamagui";
 import { Jellyfin } from "@jellyfin/sdk";
 import { getSystemApi } from "@jellyfin/sdk/lib/utils/api/system-api";
 import AsyncStorage from "@react-native-async-storage/async-storage";
@@ -9,6 +8,7 @@ import { useMutation } from "@tanstack/react-query";
 import { client } from "../../../api/queries";
 import { AsyncStorageKeys } from "../../../enums/async-storage-keys";
 import { ActivityIndicator } from "react-native";
+import { Button, TextField, View } from "react-native-ui-lib";
 
 export default function ServerAddress(): React.JSX.Element {
 
@@ -41,24 +41,17 @@ export default function ServerAddress(): React.JSX.Element {
     });
 
     return (
-        <YStack>
-            <Input 
-                fontFamily={'$body'}
+        <View>
+            <TextField 
                 placeholder="Jellyfin Server Address"
-                onChangeText={(value) => validateServerUrl(value) ?? setServerUrl(value)}
-                padding="$2"
-                borderRadius="$2">
-                </Input>
+                onChangeText={(value) => validateServerUrl(value) ?? setServerUrl(value)}>
+                </TextField>
 
             <Button 
-                fontFamily={'$body'}
                 onPress={() => serverUrlMutation.mutate(serverUrl)}
                 disabled={_.isEmpty(serverUrl) || serverUrlMutation.isPending}>
-                    { serverUrlMutation.isPending 
-                        ? <ActivityIndicator size="small"/> 
-                        : <SizableText size="$3">Connect</SizableText> 
-                    }
+                    Connect
             </Button>
-        </YStack>
+        </View>
     )
 }
