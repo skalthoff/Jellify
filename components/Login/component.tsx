0653c96ec480a27982939bade2ea99cc4c53bf72
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-15 16:17:11 -0500

    fix old import

--- components/Login/component.tsx
+++ components/Login/component.tsx
@@ -2,15 +2,14 @@ import _, { isError } from "lodash"
 import ServerAuthentication from "./helpers/server-authentication";
 import ServerAddress from "./helpers/server-address";
 import { useServer } from "../../api/queries/keychain";
-import { View } from "react-native-ui-lib";
-import { serverUrlMutation } from "../../api/mutators/storage";
+import { View } from "react-native";
 
 export default function Login(): React.JSX.Element {
 
     let { isError } = useServer;
 
     return (
-        <View useSafeArea marginH>
+        <View>
             { isError ? <ServerAddress /> : <ServerAuthentication /> }
         </View>
     );
