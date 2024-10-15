e0c499fa40caaa37690fac654143951d0e03372a
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-15 09:00:38 -0500

    can I do it this way??

--- api/queries/functions/storage.ts
+++ api/queries/functions/storage.ts
@@ -7,7 +7,7 @@ export const fetchServerUrl : Promise<string> = new Promise(async (resolve, reje
 
     console.log("Attempting to fetch server address from storage");
     
-    let url = await AsyncStorage.getItem(AsyncStorageKeys.ServerUrl)!;
+    let url = "";
 
     if (_.isEmpty(url))
         reject("No stored server address exists");
--- api/queries/keychain.ts
+++ api/queries/keychain.ts
@@ -3,6 +3,8 @@ import { QueryKeys } from "../../enums/query-keys"
 import { fetchServerUrl } from "./functions/storage";
 import _ from "lodash";
 import * as Keychain from "react-native-keychain"
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { AsyncStorageKeys } from "../../enums/async-storage-keys";
 
 export const useCredentials = useQuery({
     queryKey: [QueryKeys.Credentials],
@@ -10,10 +12,13 @@ export const useCredentials = useQuery({
 
         console.log("Attempting to use stored credentials");
 
-        let serverUrl = await fetchServerUrl;
+        let serverUrl = await AsyncStorage.getItem(AsyncStorageKeys.ServerUrl);
 
         console.debug(`REMOVE THIS::Server Url ${serverUrl}`);
 
-        return Keychain.getInternetCredentials(serverUrl!);
+        if (!_.isNull(serverUrl))
+            return Keychain.getInternetCredentials(serverUrl!);
+
+        return new Error("Unable to retrieve credentials without a server URL")
     }
 });
