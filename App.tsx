8c146b5b69134d61443e51a23f6f48474e4de4e4
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-17 23:09:32 -0500

    tamagui config change

--- App.tsx
+++ App.tsx
@@ -5,24 +5,18 @@ import "react-native-url-polyfill/auto";
 import Jellify from './components/jellify';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createTamagui, TamaguiProvider } from 'tamagui';
-import { config } from '@tamagui/config/v3';
+import defaultConfig from '@tamagui/config/v3';
 
-// you usually export this from a tamagui.config.ts file
-const tamaguiConfig = createTamagui(config)
-
-// TypeScript types across all Tamagui APIs
-type Conf = typeof tamaguiConfig
-declare module '@tamagui/core' {
-  interface TamaguiCustomConfig extends Conf {}
-}
 
 export default function App(): React.JSX.Element {
-
+  
   const queryClient = new QueryClient();
 
+  const config = createTamagui(defaultConfig);
+  
   return (
     <QueryClientProvider client={queryClient}>
-      <TamaguiProvider config={tamaguiConfig}>
+      <TamaguiProvider config={config}>
           <Jellify />
       </TamaguiProvider>
     </QueryClientProvider>
