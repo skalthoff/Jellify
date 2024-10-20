232da7c9ae606c33cf1607cedd6195643c4a7009
Violet Caulfield <violet@cosmonautical.cloud>
2024-10-20 06:29:13 -0500

    imma fix this

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,16 +1,19 @@
-import createBottomTabNavigator from "@react-navigation/bottom-tabs/lib/typescript/src/navigators/createBottomTabNavigator";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "./Home/component";
+import Player from "./Player/component";
 
 export default function Navigation(): React.JSX.Element {
 
-  const Stack = createNativeStackNavigator()
+  const RootStack = createNativeStackNavigator()
   
-  const Tab = createBottomTabNavigator();
-
     return (
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
-          <Tab.Screen name="Home" options={{ headerShown: false }} component={Home} />
-        </Stack.Navigator>
+      <RootStack.Navigator>
+      <RootStack.Group>
+        <RootStack.Screen name="Jellify" component={Home} />
+      </RootStack.Group>
+      <RootStack.Group screenOptions={{ presentation: 'modal' }}>
+        <RootStack.Screen name="Player" component={Player} />
+      </RootStack.Group>
+    </RootStack.Navigator>
     )
 }
