c4d3dd45a8ce43f1ae7a4393285f684b336ab24b
Violet Caulfield <violet@cosmonautical.cloud>
2024-11-23 08:28:56 -0600

    add icon to home tab

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from "./Home/component";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 const Tab = createBottomTabNavigator();
 
 export function Tabs() {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen 
+                name="Home" 
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="home" color={color} size={size} />
+                    )
+                }}
+            />
         </Tab.Navigator>
     )
 }
