3dc88e5fa3284c74cb421d2026ee894a8c7162b5
Violet Caulfield <violet@cosmonautical.cloud>
2024-11-24 06:27:36 -0600

    gimme a greeting too plz

--- components/Home/component.tsx
+++ components/Home/component.tsx
@@ -2,14 +2,17 @@ import { ScrollView, YStack } from "tamagui";
 import _ from "lodash";
 import { Heading } from "../helpers/text";
 import RecentlyPlayed from "./helpers/recently-played";
+import { useApiClientContext } from "../jellyfin-api-provider";
 
 
 export default function Home(): React.JSX.Element {
 
+    const { user } = useApiClientContext();
+
     return (
             <ScrollView paddingLeft={10}>
                 <YStack alignContent='flex-start'>
-                    <Heading>Hi there</Heading>
+                    <Heading>{`Hi, ${user!.name}`}</Heading>
                     
                     <RecentlyPlayed />
                 </YStack>
