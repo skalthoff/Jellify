1b1edd72377facf96225ef36651f8d26e0d06bbb
Violet Caulfield <violet@cosmonautical.cloud>
2024-11-23 19:40:40 -0600

    okokokok?

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -2,19 +2,19 @@ import { animations, tokens, themes, media, shorthands, config } from '@tamagui/
 import { createFont, createTamagui } from 'tamagui' // or '@tamagui/core'
 
 const aileronFace = {
-    100: { normal: 'Aileron UltraLight', italic: 'Aileron UltraLight Italic' },
-    200: { normal: 'Aileron Thin', italic: 'Aileron Thin Italic' },
-    300: { normal: 'Aileron Light', italic: 'Aileron Light Italic' },
-    400: { normal: 'Aileron', italic: 'Aileron Italic'} ,
-    500: { normal: 'Aileron', italic: 'Aileron Italic' },
-    600: { normal: 'Aileron SemiBold', italic: 'Aileron SemiBold Italic' },
-    700: { normal: 'Aileron Bold', italic: 'Aileron Bold Italic' },
-    800: { normal: 'Aileron Heavy', italic: 'Aileron Heavy Italic' },
-    900: { normal: 'Aileron Black', italic: 'Aileron-BlackItalic' }
+    100: { normal: 'Aileron-UltraLight', italic: 'Aileron UltraLight Italic' },
+    200: { normal: 'Aileron-Thin', italic: 'Aileron Thin Italic' },
+    300: { normal: 'Aileron-Light', italic: 'Aileron Light Italic' },
+    400: { normal: 'Aileron-Regular', italic: 'Aileron Italic'} ,
+    500: { normal: 'Aileron-Regular', italic: 'Aileron Italic' },
+    600: { normal: 'Aileron-SemiBold', italic: 'Aileron SemiBold Italic' },
+    700: { normal: 'Aileron-Bold', italic: 'Aileron Bold Italic' },
+    800: { normal: 'Aileron-Heavy', italic: 'Aileron Heavy Italic' },
+    900: { normal: 'Aileron-Black', italic: 'Aileron-BlackItalic' }
 };
 
 const bodyFont = createFont({
-    family: "Aileron",
+    family: "Aileron-Regular",
     size: config.fonts.body.size,
     lineHeight: config.fonts.body.lineHeight,
     weight: config.fonts.body.weight,
@@ -23,7 +23,7 @@ const bodyFont = createFont({
 })
 
 const headingFont = createFont({
-    family: "Aileron",
+    family: "Aileron-Black",
     size: config.fonts.heading.size,
     lineHeight: config.fonts.heading.lineHeight,
     weight: config.fonts.heading.weight,
