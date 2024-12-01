91e2bb7ac9079276118c845d7fb198794f596e16
Violet Caulfield <violet@cosmonautical.cloud>
2024-12-01 08:22:38 -0600

    okay lets build the mini player

--- components/tabs.tsx
+++ components/tabs.tsx
@@ -8,72 +8,75 @@ import Search from "./Search/component";
 import Library from "./Library/component";
 import Settings from "./Settings/component";
 import { Discover } from "./Discover/component";
+import { ZStack } from "tamagui";
 
 const Tab = createBottomTabNavigator();
 
-export function Tabs() {
+export function Tabs() : React.JSX.Element {
 
     const isDarkMode = useColorScheme() === 'dark';
 
     return (
-        <Tab.Navigator
-            screenOptions={{
-                tabBarActiveTintColor: isDarkMode ? Colors.Primary : Colors.Secondary
-            }}
-        >
-            <Tab.Screen 
-                name="Home" 
-                component={Home}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="jellyfish-outline" color={color} size={size} />
-                    ),
+        <ZStack fullscreen>
+            <Tab.Navigator
+                screenOptions={{
+                    tabBarActiveTintColor: isDarkMode ? Colors.Primary : Colors.Secondary
                 }}
-            />
+            >
+                <Tab.Screen 
+                    name="Home" 
+                    component={Home}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="jellyfish-outline" color={color} size={size} />
+                        ),
+                    }}
+                />
 
-            <Tab.Screen
-                name="Library"
-                component={Library}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({color, size }) => (
-                        <MaterialCommunityIcons name="heart-multiple-outline" color={color} size={size} />
-                    )
-                }}
-            />
+                <Tab.Screen
+                    name="Library"
+                    component={Library}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({color, size }) => (
+                            <MaterialCommunityIcons name="heart-multiple-outline" color={color} size={size} />
+                        )
+                    }}
+                />
 
-            <Tab.Screen
-                name="Search"
-                component={Search}
-                options={{
-                    tabBarIcon: ({color, size }) => (
-                        <MaterialCommunityIcons name="magnify" color={color} size={size} />
-                    )
-                }}
-            />
+                <Tab.Screen
+                    name="Search"
+                    component={Search}
+                    options={{
+                        tabBarIcon: ({color, size }) => (
+                            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+                        )
+                    }}
+                />
 
-            <Tab.Screen
-                name="Discover"
-                component={Discover}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="music-box-multiple-outline" color={color} size={size} />
-                    )
-                }}
-            />
+                <Tab.Screen
+                    name="Discover"
+                    component={Discover}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="music-box-multiple-outline" color={color} size={size} />
+                        )
+                    }}
+                />
 
-            <Tab.Screen
-                name="Settings"
-                component={Settings}
-                options={{
-                    headerShown: false,
-                    tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="dip-switch" color={color} size={size} />
-                    )
-                }}
-            />
-        </Tab.Navigator>
+                <Tab.Screen
+                    name="Settings"
+                    component={Settings}
+                    options={{
+                        headerShown: false,
+                        tabBarIcon: ({ color, size }) => (
+                            <MaterialCommunityIcons name="dip-switch" color={color} size={size} />
+                        )
+                    }}
+                />
+            </Tab.Navigator>
+        </ZStack>
     )
 }
