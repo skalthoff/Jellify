ae111106f35e07fa82a656a50a15b4dd62a5d9df
Violet Caulfield <violet@cosmonautical.cloud>
2024-12-01 06:23:52 -0600

    styling improvements, make card text pop

--- components/Global/card.tsx
+++ components/Global/card.tsx
@@ -1,16 +1,13 @@
 import React, { ReactNode, useState } from "react";
 import type { CardProps as TamaguiCardProps } from "tamagui"
-import { H3, Card as TamaguiCard, ZStack } from "tamagui";
-import { LinearGradient } from "tamagui/linear-gradient";
+import { H3, Card as TamaguiCard } from "tamagui";
 import { useApiClientContext } from "../jellyfin-api-provider";
 import { cardDimensions } from "./component.config";
-import { useImage } from "../../api/queries/image";
-import { Colors } from "../../enums/colors";
 import { getImageApi } from "@jellyfin/sdk/lib/utils/api";
 import { ImageType } from "@jellyfin/sdk/lib/generated-client/models";
 import { CachedImage } from "@georstat/react-native-image-cache";
-import { queryConfig } from "../../api/queries/query.config";
-import { Blurhash } from "react-native-blurhash";
+import invert from "invert-color"
+import { Blurhash } from "react-native-blurhash"
 
 interface CardProps extends TamaguiCardProps {
     artistName?: string;
@@ -27,6 +24,8 @@ export function Card(props: CardProps) {
     const [imageLoaded, setImageLoaded] = useState<boolean>(false); 
     const dimensions = props.artistName ? cardDimensions.artist : cardDimensions.album;
 
+    const cardTextColor = props.blurhash ? invert(Blurhash.getAverageColor(props.blurhash)!, true) : undefined;
+
     return (
         <TamaguiCard 
             elevate 
@@ -40,7 +39,7 @@ export function Card(props: CardProps) {
         >
             <TamaguiCard.Header padded>
             { props.children && (
-                <H3>{ props.children }</H3>
+                <H3 color={cardTextColor ? cardTextColor : 'unset'}>{ props.children }</H3>
             )}
             </TamaguiCard.Header>
             <TamaguiCard.Footer padded>
@@ -65,7 +64,8 @@ export function Card(props: CardProps) {
                             { ...dimensions})
                     } 
                     imageStyle={{
-                        ...dimensions
+                        ...dimensions,
+                        borderRadius: 25
                     }}
                     onLoadEnd={() => setImageLoaded(true)}
                 />
