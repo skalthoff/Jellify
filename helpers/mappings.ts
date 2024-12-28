63c13efdd5f3e9a22df4cb83fc933222adff0e3a
Violet Caulfield <violet@cosmonautical.cloud>
2024-12-28 09:13:31 -0600

    player logic

--- helpers/mappings.ts
+++ helpers/mappings.ts
@@ -3,15 +3,32 @@ import { JellifyTrack } from "../types/JellifyTrack";
 import { TrackType } from "react-native-track-player";
 import { Api } from "@jellyfin/sdk";
 import { QueuingType } from "../enums/queuing-type";
+import querystring from "querystring"
+
+const container = "opus,mp3,aac,m4a,flac,webma,webm,wav,ogg,mpa,wma";
+
+// TODO: Make this configurable
+const transcodingContainer = "m4a";
 
 export function mapDtoToTrack(api: Api, item: BaseItemDto, queuingType?: QueuingType) {
 
+    console.log(`Mapping track for ${api.basePath} server`)
+
+    const urlParams = {
+        "Container": container,
+        "TranscodingContainer": transcodingContainer,
+        "TranscodingProtocol": "hls",
+        "EnableRemoteMedia": true,
+        "EnableRedirection": true,
+        "api_key": api.accessToken,
+        "StartTimeTicks": 0,
+    }
+
     return {
-        url: `${api.basePath}/Audio/${item.Id!}/universal?TranscodingProtocol=hls?EnableRemoteMedia=true?EnableRedirection=true`,
+        url: `${api.basePath}/Audio/${item.Id!}/universal?${querystring.stringify(urlParams)}`,
         type: TrackType.HLS,
         headers: {
             "X-Emby-Token": api.accessToken
-
         },
         title: item.Name,
         album: item.Album,
