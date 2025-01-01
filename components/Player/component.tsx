f22c5d13ae680d88727ba5ee84047bfd2e345674
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-01 12:46:05 -0600

    update player screen

--- components/Player/component.tsx
+++ components/Player/component.tsx
@@ -1,8 +1,7 @@
-import { Event, useActiveTrack, useTrackPlayerEvents } from "react-native-track-player";
+import { Event, useTrackPlayerEvents } from "react-native-track-player";
 import { handlePlayerError } from "./helpers/error-handlers";
 import { usePlayerContext } from "../../player/provider";
-import { JellifyTrack } from "../../types/JellifyTrack";
-import { Stack, YStack } from "tamagui";
+import { Stack as HStack, YStack } from "tamagui";
 import { CachedImage } from "@georstat/react-native-image-cache";
 import { useApiClientContext } from "../jellyfin-api-provider";
 import { getImageApi } from "@jellyfin/sdk/lib/utils/api";
@@ -10,7 +9,6 @@ import { ImageType } from "@jellyfin/sdk/lib/generated-client/models";
 import { queryConfig } from "../../api/queries/query.config";
 import { Text } from "../Global/text";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useEffect } from "react";
 
 /**
  * Events subscribed to within RNTP
@@ -34,28 +32,38 @@ export default function Player(): React.JSX.Element {
             { nowPlaying && (
                 <YStack alignItems="center">
 
-                    <CachedImage
-                        source={getImageApi(apiClient!)
-                            .getItemImageUrlById(
-                                nowPlaying!.AlbumId,
-                                ImageType.Primary,
-                                { ...queryConfig.playerArtwork }
-                            )
-                        }
-                        imageStyle={{
-                            width: 400,
-                            height: 400,
-                            borderRadius: 2
-                        }}
-                    />
-                    <Stack justifyContent="space-between">
+                    <HStack alignContent="center">
 
-                        <Stack alignItems="flex-start" justifyContent="flex-start">
+                        <CachedImage
+                            source={getImageApi(apiClient!)
+                                .getItemImageUrlById(
+                                    nowPlaying!.AlbumId,
+                                    ImageType.Primary,
+                                    { ...queryConfig.playerArtwork }
+                                )
+                            }
+                            imageStyle={{
+                                width: 400,
+                                height: 400,
+                                borderRadius: 2
+                            }}
+                            />
+                    </HStack>
+
+                    <HStack justifyContent="space-between">
+
+                        <YStack alignItems="flex-start" justifyContent="flex-start" flex={3}>
                             <Text>{nowPlaying?.title ?? "Untitled Track"}</Text>
                             <Text bold>{nowPlaying?.artist ?? "Unknown Artist"}</Text>
-                        </Stack>
+                        </YStack>
 
-                    </Stack>
+                        <HStack flex={1}>
+                            {/* Buttons for favorites, song menu go here */}
+
+                        </HStack>
+
+                    </HStack>
+
                     
                     </YStack>
             )}
