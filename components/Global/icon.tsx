b3b2ebcd2d65fdf840c3fa24fdd4a92f57efbf1d
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-01 13:41:33 -0600

    iconography

--- components/Global/icon.tsx
+++ components/Global/icon.tsx
@@ -1,26 +1,24 @@
 import React from "react"
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons"
-import { useTheme } from "tamagui"
 import { Colors } from "../../enums/colors"
 
 const iconDimensions = {
-    width: 50,
-    height: 50
+    width: 75,
+    height: 75
 }
 
 const largeDimensions = {
-    width: 200,
-    height: 200
+    width: 150,
+    height: 150
 }
 
 export default function Icon({ name, onPress, large }: { name: string, onPress?: Function, large?: boolean }) : React.JSX.Element {
     
-    const theme = useTheme();
     let dimensions = large ? largeDimensions : iconDimensions
     
     return (
         <MaterialCommunityIcons 
-            color="#ffffff"
+            color={Colors.White}
             name={name} 
             onPress={() => {
                 if (onPress)
