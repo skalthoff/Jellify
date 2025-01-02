0b77f2abb17780b9d70deea3f6fb66469611fc0d
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-02 06:21:35 -0600

    make album tracklist actually readable

--- components/Album/component.tsx
+++ components/Album/component.tsx
@@ -1,7 +1,7 @@
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StackParamList } from "../types";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { ScrollView, Stack } from "tamagui";
+import { ScrollView, Separator, Stack, View, YStack } from "tamagui";
 import { CachedImage } from "@georstat/react-native-image-cache";
 import { getImageApi } from "@jellyfin/sdk/lib/utils/api";
 import { useApiClientContext } from "../jellyfin-api-provider";
@@ -28,54 +28,55 @@ export default function Album(props: AlbumProps): React.JSX.Element {
     const { data: tracks, isLoading } = useAlbumTracks(props.albumId, apiClient!);
 
     return (
-        <SafeAreaView>
-            <ScrollView
-                alignContent="center"
-                contentInsetAdjustmentBehavior="automatic"
-                >
-                    <CachedImage
-                        source={getImageApi(apiClient!)
-                            .getItemImageUrlById(
-                                props.albumId,
-                                ImageType.Primary,
-                                { ...queryConfig.images})}
-                        imageStyle={{
-                            position: "relative",
-                            width: 300,
-                            height: 300,
-                            borderRadius: 2
-                        }}
-                    />
+        <ScrollView alignItems="center">
+                <CachedImage
+                    source={getImageApi(apiClient!)
+                        .getItemImageUrlById(
+                            props.albumId,
+                            ImageType.Primary,
+                            { ...queryConfig.images})}
+                    imageStyle={{
+                        position: "relative",
+                        width: 300,
+                        height: 300,
+                        borderRadius: 2
+                    }}
+                />
 
+                <YStack>
                     <H4>{ props.albumName ?? "Untitled Album" }</H4>
-                    <FlatList
-                        contentContainerStyle={{
-                            flexGrow: 1,
-                            justifyContent: "center",
-                            alignItems: "flex-start"
-                        }}
-                        data={tracks}
-                        numColumns={1}
-                        renderItem={({ item: track, index }) => {
-                            return (
-                                <Stack onPress={async (track) => {
-                                    await resetQueue(false)
-                                    await addToQueue(tracks!.map((track) => mapDtoToTrack(apiClient!, sessionId, track)));
-                                    play(index);
-                                }}>
-                                    <Stack>
+                </YStack>
+                <FlatList
+                    contentContainerStyle={{
+                        flexGrow: 1,
+                    }}
+                    data={tracks}
+                    numColumns={1}
+                    renderItem={({ item: track, index }) => {
+                        return (
+                            <View>
+                                <Separator />
+                                <Stack 
+                                    flex={1}
+                                    onPress={async (track) => {
+                                        await resetQueue(false)
+                                        await addToQueue(tracks!.map((track) => mapDtoToTrack(apiClient!, sessionId, track)));
+                                        play(index);
+                                    }
+                                }>
+                                    <Stack alignItems="flex-start">
                                         <Text>{ track.IndexNumber?.toString() ?? "" }</Text>
                                         <Text>{ track.Name ?? "Untitled Track" }</Text>
                                     </Stack>
 
-                                    <Stack>
+                                    <Stack alignItems="flex-end">
                                         <Text>{ track.RunTimeTicks?.toString() ?? "" }</Text>
                                     </Stack>
                                 </Stack>
-                            )
+                            </View>
+                        )
 
-                        }}/>
-                </ScrollView>
-        </SafeAreaView>
+                    }}/>
+            </ScrollView>
     )
 }
