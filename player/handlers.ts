c91e0f9b94f505082f6974688313e34c7f3c96ab
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-09 09:55:43 -0600

    omfg

--- player/handlers.ts
+++ player/handlers.ts
@@ -1,28 +1,31 @@
 import { Progress, State } from "react-native-track-player";
 import { JellifyTrack } from "../types/JellifyTrack";
 import { PlaystateApi } from "@jellyfin/sdk/lib/generated-client/api/playstate-api";
-import { convertSecondsToRunTimeTicks } from "@/helpers/runtimeticks";
 
 export async function handlePlaybackState(sessionId: string, playstateApi: PlaystateApi, track: JellifyTrack, state: State) {
     switch (state) {            
         case (State.Playing) : {
+            console.debug("Report playback started")
             await playstateApi.reportPlaybackStart({
                 playbackStartInfo: {
                     SessionId: sessionId,
                     ItemId: track.ItemId
                 }
-            })
+            });
+            break;
         }
         
         case (State.Ended) :
         case (State.Paused) :
         case (State.Stopped) : {
+            console.debug("Report playback stopped")
             await playstateApi.reportPlaybackStopped({
                 playbackStopInfo: {
                     SessionId: sessionId,
                     ItemId: track.ItemId
                 }
             })
+            break;
         }
 
         default : {
