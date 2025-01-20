3bce5d0053c20e2be9a1b13aaca779dd81e482a1
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-19 19:41:44 -0600

    fix scroll to index in queue causing a crash

--- components/Player/screens/queue.tsx
+++ components/Player/screens/queue.tsx
@@ -3,10 +3,11 @@ import { StackParamList } from "@/components/types";
 import { usePlayerContext } from "@/player/provider";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { FlatList } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaView, useSafeAreaFrame } from "react-native-safe-area-context";
 
 export default function Queue({ navigation }: { navigation: NativeStackNavigationProp<StackParamList>}): React.JSX.Element {
 
+    const { width } = useSafeAreaFrame();
     const { queue, useSkip, nowPlaying } = usePlayerContext();
 
     const scrollIndex = queue.findIndex(queueItem => queueItem.item.Id! === nowPlaying!.item.Id!)
@@ -16,6 +17,9 @@ export default function Queue({ navigation }: { navigation: NativeStackNavigatio
             <FlatList
                 data={queue}
                 extraData={nowPlaying}
+                getItemLayout={(data, index) => (
+                    { length: width / 9, offset: width / 9 * index, index}
+                )}
                 initialScrollIndex={scrollIndex !== -1 ? scrollIndex: 0}
                 numColumns={1}
                 renderItem={({ item: queueItem, index }) => {
