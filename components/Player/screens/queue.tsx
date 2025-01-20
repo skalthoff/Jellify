c4d67816fd7b3ff6818df00c0138798ba263b500
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-19 19:34:22 -0600

    fix queue scroll to index

--- components/Player/screens/queue.tsx
+++ components/Player/screens/queue.tsx
@@ -1,22 +1,27 @@
 import Track from "@/components/Global/components/track";
+import { StackParamList } from "@/components/types";
 import { usePlayerContext } from "@/player/provider";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { FlatList } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function Queue(): React.JSX.Element {
+export default function Queue({ navigation }: { navigation: NativeStackNavigationProp<StackParamList>}): React.JSX.Element {
 
     const { queue, useSkip, nowPlaying } = usePlayerContext();
+
+    const scrollIndex = queue.findIndex(queueItem => queueItem.item.Id! === nowPlaying!.item.Id!)
 
     return (
         <SafeAreaView edges={["right", "left"]}>
             <FlatList
                 data={queue}
                 extraData={nowPlaying}
-                initialScrollIndex={queue.indexOf(nowPlaying!)}
+                initialScrollIndex={scrollIndex !== -1 ? scrollIndex: 0}
                 numColumns={1}
                 renderItem={({ item: queueItem, index }) => {
                     return (
                         <Track
+                            navigation={navigation}
                             track={queueItem.item}
                             tracklist={queue.map((track) => track.item)}
                             index={index}
