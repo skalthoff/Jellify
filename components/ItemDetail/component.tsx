01c41a27f76fffd81c84a7e007f65c01abc2a429
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-21 10:48:30 -0600

    dismiss item modal

--- components/ItemDetail/component.tsx
+++ components/ItemDetail/component.tsx
@@ -64,6 +64,7 @@ export default function ItemDetail({
                         color={Colors.Primary}
                         onPress={() => {
                             if (item.ArtistItems) {
+                                navigation.goBack(); // Dismiss modal if exists
                                 navigation.navigate("Artist", {
                                     artist: item.ArtistItems[0]
                                 });
