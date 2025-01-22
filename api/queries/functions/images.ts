ec3cbfc75f55e9543b9f3108012e146c47d39d0d
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-22 04:44:12 -0600

    idk maybe?

--- api/queries/functions/images.ts
+++ api/queries/functions/images.ts
@@ -1,31 +1,7 @@
-import { Api } from "@jellyfin/sdk/lib/api"
 import { ImageFormat, ImageType } from "@jellyfin/sdk/lib/generated-client/models"
 import { getImageApi } from "@jellyfin/sdk/lib/utils/api"
 import _ from "lodash"
-import { queryConfig } from "../query.config"
 import Client from "../../../api/client"
-
-
-
-
-export function fetchImage(api: Api, itemId: string, imageType?: ImageType) : Promise<string> {
-    return new Promise(async (resolve) => {
-        let imageResponse = await api.axiosInstance
-            .get(getImageApi(api).getItemImageUrlById(
-                itemId, 
-                imageType, 
-                { 
-                    format: queryConfig.images.format, 
-                    fillHeight: queryConfig.images.fillHeight,
-                    fillWidth: queryConfig.images.fillWidth
-                }
-            ))
-
-        console.debug(convertFileToBase64(imageResponse.data));
-        console.debug(typeof imageResponse.data)
-        resolve(convertFileToBase64(imageResponse.data));
-    });
-}
 
 export function fetchItemImage(itemId: string, imageType?: ImageType, width?: number) {
     
@@ -37,8 +13,9 @@ export function fetchItemImage(itemId: string, imageType?: ImageType, width?: nu
         })
         .then(async (response) => {
             console.log(response.data)
-
-            return convertFileToBase64(response.data)
+            return new Blob(response.data)
+        }).then(async (blob) => {
+            return await blobToBase64(blob)
         });
 }
 
@@ -46,11 +23,10 @@ function base64toJpeg(encode: string) : string {
     return `data:image/jpeg;base64,${encode}`;
 }
 
-function convertFileToBase64(file: string): string {
-    console.debug("Converting file to base64", file)
-    let encode =  base64toJpeg(Buffer.from(file, 'binary').toString('base64'));
-
-    console.debug(encode);
-
-    return encode;
-}
+function blobToBase64(blob : Blob) : Promise<string> {
+    return new Promise((resolve, _) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result.toString());
+      reader.readAsDataURL(blob);
+    });
+  }
