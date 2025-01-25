9f7c7db82e0be888d95b4d46c9efc809aa8e42ac
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-25 17:41:18 -0600

    am I missing that?

--- components/CarPlay/Home.tsx
+++ components/CarPlay/Home.tsx
@@ -5,6 +5,7 @@ const recentArtists = fetchRecentlyPlayedArtists()
 
 const CarPlayHome : GridTemplate = new GridTemplate({
     title: "Home",
+    tabTitle: "Home",
     buttons: Array.from({ length: 8 }).map((_, i) => ({
         id: `BUTTON_${i}`,
         image: require("../../assets/icon_60pt_3x.jpg"),
