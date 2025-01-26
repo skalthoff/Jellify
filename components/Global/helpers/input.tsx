8c2fd6fd2ad059e17cf0c6a16e17e7b49a6f803a
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-26 09:30:05 -0600

    bruh

--- components/Global/helpers/input.tsx
+++ components/Global/helpers/input.tsx
@@ -8,11 +8,7 @@ export default function Input(props: InputProps): React.JSX.Element {
 
     return (
         <TamaguiInput 
-            placeholder={props.placeholder}
-            onChangeText={props.onChangeText} 
-            value={props.value}
-            flexGrow={props.flexGrow ? 1 : "unset"}
-            secureTextEntry={props.secureTextEntry}
+            {...props}
             clearButtonMode="always"
         />
     )
