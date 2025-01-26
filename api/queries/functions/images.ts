54d87a6c19af08c9a629895a72425156897c2b78
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-26 07:41:59 -0600

    Image fetching stuff

--- api/queries/functions/images.ts
+++ api/queries/functions/images.ts
@@ -1,22 +1,33 @@
-import { ImageFormat, ImageType } from "@jellyfin/sdk/lib/generated-client/models"
+import { ImageType } from "@jellyfin/sdk/lib/generated-client/models"
 import { getImageApi } from "@jellyfin/sdk/lib/utils/api"
 import _ from "lodash"
 import Client from "../../../api/client"
 import { QueryConfig } from "../query.config";
+import { Dirs, FileSystem } from 'react-native-file-access'
 
-export function fetchItemImage(itemId: string, imageType?: ImageType, width?: number, height?: number) {
+export async function fetchItemImage(itemId: string, imageType?: ImageType, width?: number, height?: number) {
     
-    return getImageApi(Client.api!)
-        .getItemImage({ 
-            itemId, 
-            imageType: imageType ? imageType : ImageType.Primary,
-            width: width ? Math.ceil(width * 2) : QueryConfig.playerArtwork.width,
-            height: height ? Math.ceil(height * 2) : QueryConfig.playerArtwork.height
-        }, {
-            responseType: 'blob'
-        })
-        .then((response) => {
-            console.log(response)
-            return URL.createObjectURL(response.data)
-        });
+    if (await FileSystem.exists(`${Dirs.CacheDir}/Images/${imageType ?? ImageType.Primary}/${itemId}`))
+        return FileSystem.readFile(getImagePath(itemId, imageType, width, height))
+
+    return FileSystem.fetch(
+        getImageApi(Client.api!)
+            .getItemImageUrlById(
+                itemId, 
+                imageType ?? ImageType.Primary, 
+                { 
+                    width: width ? Math.ceil(width * 2) : QueryConfig.playerArtwork.width,
+                    height: height ? Math.ceil(height * 2) : QueryConfig.playerArtwork.height
+                }
+            ),
+            {
+                path: getImagePath(itemId, imageType, width, height),
+            }
+    ).then((result) => {
+        return result.url
+    })
+}
+
+function getImagePath(itemId: string, imageType?: ImageType, width?: number, height?: number) {
+    return `${Dirs.CacheDir}/Images/${imageType ?? ImageType.Primary}/${itemId}-${width ?? QueryConfig.playerArtwork.width}-${height ?? QueryConfig.playerArtwork.height}`;
 }
--- api/queries/image.ts
+++ api/queries/image.ts
@@ -2,12 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { QueryKeys } from "../../enums/query-keys";
 import { fetchItemImage } from "./functions/images";
 import { ImageType } from "@jellyfin/sdk/lib/generated-client/models";
-import { QueryConfig } from "./query.config";
 
 export const useItemImage = (itemId: string, imageType?: ImageType, width?: number, height?: number) => useQuery({
     queryKey: [QueryKeys.ItemImage, itemId, imageType, width, height],
     queryFn: () => fetchItemImage(itemId, imageType, width, height),
-    staleTime: QueryConfig.staleTime.oneWeek,
     gcTime: Infinity,
     retry: 3
 });
