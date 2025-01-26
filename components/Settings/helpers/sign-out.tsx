c1f3d252f14667903ea638434b07e7a1686ca485
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-26 08:13:32 -0600

    fix bug on signing out

--- components/Settings/helpers/sign-out.tsx
+++ components/Settings/helpers/sign-out.tsx
@@ -2,17 +2,13 @@ import React from "react";
 import Button from "../../Global/helpers/button";
 import { stop } from "react-native-track-player/lib/src/trackPlayer";
 import Client from "../../../api/client";
-import { useJellifyContext } from "../../../components/provider";
 
 export default function SignOut(): React.JSX.Element {
-
-    const { setLoggedIn } = useJellifyContext()
 
     return (
         <Button onPress={() => {
             stop();
             Client.signOut();
-            setLoggedIn(false);
         }}>
             Sign Out
         </Button>
