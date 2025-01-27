210fe1ded266774340561628c08937bc803b00cd
Violet Caulfield <violet@cosmonautical.cloud>
2025-01-26 19:02:51 -0600

    rollback queuing thing

--- components/Player/screens/queue.tsx
+++ components/Player/screens/queue.tsx
@@ -2,24 +2,26 @@ import Track from "../../../components/Global/components/track";
 import { StackParamList } from "../../../components/types";
 import { usePlayerContext } from "../../../player/provider";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { FlatList, SectionList } from "react-native";
+import { FlatList } from "react-native";
 import { SafeAreaView, useSafeAreaFrame } from "react-native-safe-area-context";
 
 export default function Queue({ navigation }: { navigation: NativeStackNavigationProp<StackParamList>}): React.JSX.Element {
 
     const { width } = useSafeAreaFrame();
-    const { getQueueSectionData, queue, useSkip, nowPlaying } = usePlayerContext();
+    const { queue, useSkip, nowPlaying } = usePlayerContext();
 
     const scrollIndex = queue.findIndex(queueItem => queueItem.item.Id! === nowPlaying!.item.Id!)
 
     return (
         <SafeAreaView edges={["right", "left"]}>
-            <SectionList
-                sections={getQueueSectionData()}
+            <FlatList
+                data={queue}
+                extraData={nowPlaying}
                 getItemLayout={(data, index) => (
                     { length: width / 9, offset: width / 9 * index, index}
                 )}
                 initialScrollIndex={scrollIndex !== -1 ? scrollIndex: 0}
+                numColumns={1}
                 renderItem={({ item: queueItem, index }) => {
                     return (
                         <Track
