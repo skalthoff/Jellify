23c305263b448550119fbf57a04f35adacfbbe79
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-03 06:52:38 -0600

    Fix build
    add some downloads logic

--- api/queries/functions/downloads.ts
+++ api/queries/functions/downloads.ts
@@ -1,11 +1,26 @@
+import { Dirs, FileSystem } from "react-native-file-access";
 import Client from "../../../api/client";
 import { getLibraryApi } from "@jellyfin/sdk/lib/utils/api";
 
 export async function downloadTrack(itemId: string) : Promise<void> {
+
+    // Make sure downloads folder exists, create if it doesn't
+    if (!(await FileSystem.exists(`${Dirs.DocumentDir}/downloads`)))
+        await FileSystem.mkdir(`${Dirs.DocumentDir}/downloads`)
+
     getLibraryApi(Client.api!)
         .getDownload({
             itemId
         }, {
             'responseType': 'blob'
         })
+        .then(async (response) => {
+            if (response.status < 300) {
+                await FileSystem.writeFile("", response.data)
+            }
+        })
+}
+
+function getTrackFilePath(itemId: string) {
+    return `${Dirs.DocumentDir}/downloads/${itemId}`
 }
