e5baeebf23cf9dd4d5580b3dae56fa796cd34c90
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-09 08:05:01 -0600

    fix build

--- components/Favorites/component.tsx
+++ components/Favorites/component.tsx
@@ -1,8 +1,8 @@
 import { FlatList } from "react-native";
 import { SafeAreaView, useSafeAreaFrame } from "react-native-safe-area-context";
-import Categories from "./categories";
-import IconCard from "../../../components/Global/helpers/icon-card";
-import { StackParamList } from "../../../components/types";
+import Categories from "../categories";
+import IconCard from "../../components/Global/helpers/icon-card";
+import { StackParamList } from "../../components/types";
 import { RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
