a416cd0ccb8c3c5e7d3827a330655da26dbf5c11
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-09 13:49:47 -0600

    idk

--- components/Player/screens/index.tsx
+++ components/Player/screens/index.tsx
@@ -78,7 +78,7 @@ export default function PlayerScreen({
 
                     <XStack>
 
-                        <YStack flex={1} justifyContent="flex-end">
+                        <YStack flex={1}>
                             <Icon
                                 name="chevron-down"
                                 onPress={() => {
