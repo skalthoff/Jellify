249d3ce4e80fad2aa4a752caffa0e165990c23c4
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-09 08:53:28 -0600

    fix crash on skipping tracks

--- components/Player/screens/index.tsx
+++ components/Player/screens/index.tsx
@@ -154,7 +154,7 @@ export default function PlayerScreen({ navigation }: { navigation: NativeStackNa
                         {/* playback progress goes here */}
                         <HorizontalSlider 
                             value={progressState}
-                            max={progress?.duration ?? 1}
+                            max={progress && progress.duration > 0 ? progress.duration : 1}
                             width={width / 1.1}
                             props={{
                                 // If user swipes off of the slider we should seek to the spot
