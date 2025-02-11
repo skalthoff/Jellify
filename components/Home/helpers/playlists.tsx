e561b3a70be563764aac7113a8b01292dd6eae82
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-10 21:45:34 -0600

    flex that create button

--- components/Home/helpers/playlists.tsx
+++ components/Home/helpers/playlists.tsx
@@ -17,7 +17,7 @@ export default function Playlists({ navigation }: { navigation: NativeStackNavig
             <XStack justifyContent="space-between" alignContent="center" marginHorizontal={"$2"}>
                 <H2>Your Playlists</H2>
 
-                <XStack justifyContent="center" alignContent="center" marginTop={7}>
+                <XStack justifyContent="center" alignContent="flex-end" marginTop={7}>
                     <Icon name="plus-circle-outline" color={getToken("$color.telemagenta")} />
                 </XStack>
             </XStack>
