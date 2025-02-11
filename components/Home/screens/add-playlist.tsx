1500f402451131f321ae2a72e91b05d5c0a73b5c
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-10 22:37:16 -0600

    safe area?

--- components/Home/screens/add-playlist.tsx
+++ components/Home/screens/add-playlist.tsx
@@ -1,14 +1,14 @@
 import { Label } from "../../../components/Global/helpers/text";
 import Input from "../../../components/Global/helpers/input";
 import React from "react";
-import { View } from "tamagui";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 
 export default function AddPlaylist() : React.JSX.Element {
     return (
-        <View>
+        <SafeAreaView>
             <Label size="$2" htmlFor="name">Name</Label>
             <Input id="name" />
-        </View>
+        </SafeAreaView>
     )
 }
