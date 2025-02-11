4adc6e8a8393004abcb98abc115a1f6ed0ebd3e2
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-11 15:29:50 -0600

    omfg

--- constants/query-client.ts
+++ constants/query-client.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            gcTime: (1000 * 60 * 24 * 24) * 5 // 5 days
+            staleTime: (1000 * 60 * 24 * 1) * 1 // 1 hour
         }
     }
 });
