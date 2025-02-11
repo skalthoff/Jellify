5582796b832d1fbe5faa3310acbec7652af4042a
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-11 15:49:28 -0600

    update query client stale time comment

--- constants/query-client.ts
+++ constants/query-client.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             gcTime: (1000 * 60 * 60), // 1 hour
-            staleTime: (1000 * 60 * 30 * 1) * 1 // 1 hour
+            staleTime: (1000 * 60 * 30) // 30 minutes
         }
     }
 });
