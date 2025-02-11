450e577ae3ca744f58753dfc5950659838442f50
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-10 22:40:00 -0600

    add playlist component layout fixes

--- components/Home/screens/add-playlist.tsx
+++ components/Home/screens/add-playlist.tsx
@@ -2,13 +2,14 @@ import { Label } from "../../../components/Global/helpers/text";
 import Input from "../../../components/Global/helpers/input";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { View } from "tamagui";
 
 
 export default function AddPlaylist() : React.JSX.Element {
     return (
-        <SafeAreaView>
+        <View marginHorizontal={"$2"}>
             <Label size="$2" htmlFor="name">Name</Label>
             <Input id="name" />
-        </SafeAreaView>
+        </View>
     )
 }
