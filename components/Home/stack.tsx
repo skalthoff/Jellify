8d2c8ff7508963dea194edf27a203602cb1a17a4
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-10 22:39:13 -0600

    add playlist sheet

--- components/Home/stack.tsx
+++ components/Home/stack.tsx
@@ -66,16 +66,16 @@ export default function Home(): React.JSX.Element {
                 </HomeStack.Group>
 
                 {/* https://www.reddit.com/r/reactnative/comments/1dgktbn/comment/lxd23sj/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button */}
-                <HomeStack.Group screenOptions={{ presentation: 'formSheet', sheetInitialDetentIndex: 1, sheetAllowedDetents: [0.25, 0.5] }}>
+                <HomeStack.Group screenOptions={{ 
+                    presentation: 'formSheet', 
+                    sheetInitialDetentIndex: 0, 
+                    sheetAllowedDetents: [0.25] 
+                }}>
                     <HomeStack.Screen
                         name="AddPlaylist"
                         component={AddPlaylist}
                         options={{
                             title: "Add Playlist",
-                            headerLargeTitle: true,
-                            headerLargeTitleStyle: {
-                                fontFamily: 'Aileron-Bold'
-                            }
                         }}
                     />
                 </HomeStack.Group>
