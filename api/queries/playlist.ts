38a8d0db5b4ed19521f5a53e0e28e21bd1043d62
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-12 23:14:55 -0600

    update query

--- api/queries/playlist.ts
+++ api/queries/playlist.ts
@@ -10,7 +10,7 @@ export const useFavoritePlaylists = () => useQuery({
 });
 
 export const useUserPlaylists = (sortBy: ItemSortBy[] = []) => useQuery({
-    queryKey: [QueryKeys.UserPlaylists],
+    queryKey: [QueryKeys.UserPlaylists, sortBy],
     queryFn: () => fetchUserPlaylists(sortBy)
 });
 
