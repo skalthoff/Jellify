473a72435c078633116913e170a903428e097844
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-12 23:38:42 -0600

    fix build

--- api/mutations/functions/item.ts
+++ api/mutations/functions/item.ts
@@ -1,4 +1,4 @@
-import Client from "@/api/client";
+import Client from "../../../api/client";
 import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import { getPlaystateApi } from "@jellyfin/sdk/lib/utils/api";
 
