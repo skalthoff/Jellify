bbe074b1adb3b131db9409b95350b4d72382cea6
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-13 16:24:39 -0600

    native?

--- components/jellify.tsx
+++ components/jellify.tsx
@@ -16,7 +16,7 @@ export default function Jellify(): React.JSX.Element {
 
   return (
     <PortalProvider shouldAddRootHost>
-      <ToastProvider>
+      <ToastProvider native>
         <JellifyProvider>
           <App />
         </JellifyProvider>
