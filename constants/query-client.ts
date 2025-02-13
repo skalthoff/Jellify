5b54a4bee8be45227c9ae9388a43b6db5e4b710d
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-13 17:05:17 -0600

    fix npm

--- constants/query-client.ts
+++ constants/query-client.ts
@@ -3,7 +3,10 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            gcTime: (1000 * 60 * 5), // 5 minutes
+            //@ts-ignore
+            cacheTime: (1000 * 60 * 60) * 24, // 1 day 
+
+            gcTime: (1000 * 60 * 5), // 5 minutes,
             staleTime: (1000 * 60 * 5), // 5 minutes,
         }
     }
