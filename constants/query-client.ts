c09278e4c8574a62941d4d4fea2f00629eae3f48
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-14 14:32:06 -0600

    query client adjustments

--- constants/query-client.ts
+++ constants/query-client.ts
@@ -3,8 +3,8 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            gcTime: (1000 * 60 * 60 * 24), // 1 day,
-            staleTime: (1000 * 60 * 10), // 10 minutes,
+            gcTime: (1000 * 60 * 60 * 24) * 5, // 5 days, for maximum cache-age
+            staleTime: (1000 * 60 * 1), // 1 minute,
         }
     }
 });
