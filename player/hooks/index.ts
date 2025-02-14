1702f2253cd01fe88e317bca2b7f3ffd5397d9c5
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-14 05:02:50 -0600

    update rntp

--- player/hooks/index.ts
+++ player/hooks/index.ts
@@ -20,7 +20,7 @@ export const useSetupPlayer = () => useQuery({
     queryFn: () => {
         return TrackPlayer.setupPlayer({
             autoHandleInterruptions: true,
-            maxCacheSize: 1000 * 500, // 500MB
+            maxCacheSize: 1000 * 2500, // 250MB, TODO make this adjustable
             iosCategory: IOSCategory.Playback,
             iosCategoryOptions: [
                 IOSCategoryOptions.AllowAirPlay,
