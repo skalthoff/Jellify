4204fd295ece31591508c4d8de5f4bd87eb17948
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-14 14:01:48 -0600

    apparently I'm supposed to do this?

--- constants/query-client.ts
+++ constants/query-client.ts
@@ -3,7 +3,7 @@ import { QueryClient } from "@tanstack/react-query";
 export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
-            gcTime: (1000 * 30), // 30 seconds,
+            gcTime: (1000 * 60 * 60 * 24), // 1 day,
             staleTime: (1000 * 60 * 10), // 10 minutes,
         }
     }
