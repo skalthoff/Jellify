c636a173be49448c546dbf2c8863c60b0ce573a4
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-14 19:41:20 -0600

    query client adjustments

--- constants/query-client.ts
+++ constants/query-client.ts
@@ -4,7 +4,7 @@ export const queryClient = new QueryClient({
     defaultOptions: {
         queries: {
             gcTime: (1000 * 60 * 60 * 24) * 5, // 5 days, for maximum cache-age
-            staleTime: (1000 * 60 * 10), // 10 minutes,
+            staleTime: (1000 * 60 * 30), // 30 minutes, this can be refreshed manually anyways
         }
     }
 });
