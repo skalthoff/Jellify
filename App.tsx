287c75cc4ce514b440013e653cfa15099fbbbae0
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-15 07:49:20 -0600

    fix build

--- App.tsx
+++ App.tsx
@@ -9,49 +9,12 @@ import jellifyConfig from './tamagui.config';
 import { clientPersister } from './constants/storage';
 import { queryClient } from './constants/query-client';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { QueryKeys } from './enums/query-keys';
-import { useQuery } from '@tanstack/react-query';
-import TrackPlayer, { IOSCategory, IOSCategoryOptions } from 'react-native-track-player';
-import { CAPABILITIES } from './player/constants';
 
 // export const backgroundRuntime = createWorkletRuntime('background');
 
 export default function App(): React.JSX.Element {
   
   const isDarkMode = useColorScheme() === 'dark';
-
-  const { isSuccess: isPlayerReady } = useQuery({
-      queryKey: [QueryKeys.Player],
-      queryFn: async () => {
-          await TrackPlayer.setupPlayer({
-              autoHandleInterruptions: true,
-              maxCacheSize: 1000 * 100, // 100MB, TODO make this adjustable
-              iosCategory: IOSCategory.Playback,
-              iosCategoryOptions: [
-                  IOSCategoryOptions.AllowAirPlay,
-                  IOSCategoryOptions.AllowBluetooth,
-              ]
-          });
-          
-          return await TrackPlayer.updateOptions({
-                  progressUpdateEventInterval: 1,
-                  capabilities: CAPABILITIES,
-                  notificationCapabilities: CAPABILITIES,
-                  compactCapabilities: CAPABILITIES,
-                  // ratingType: RatingType.Heart,
-                  // likeOptions: {
-                  //     isActive: false,
-                  //     title: "Favorite"
-                  // },
-                  // dislikeOptions: {
-                  //     isActive: true,
-                  //     title: "Unfavorite"
-                  // }
-          });
-      },
-      retry: 0,
-      staleTime: 1000 * 60 * 60 * 24 * 7 // 7 days
-  });
 
   return (
     <PersistQueryClientProvider 
