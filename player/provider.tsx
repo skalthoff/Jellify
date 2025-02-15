7ae3fd82e7aa366b23ff42feacb72796362f7f1a
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-15 06:46:12 -0600

    i reall yneed ths

--- player/provider.tsx
+++ player/provider.tsx
@@ -265,8 +265,8 @@ const PlayerContextInitializer = () => {
                     // }
             });
         },
-        // retry: 0,
-        // staleTime: 1000 * 60 * 60 * 24 * 7 // 7 days
+        retry: 0,
+        staleTime: 1000 * 60 * 60 * 24 * 7 // 7 days
     });
     
     const { state: playbackState } = usePlaybackState();
