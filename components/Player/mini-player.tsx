5e4ad72e0029663d54abd0224a595d50d5504776
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-16 09:55:01 -0600

    uhh

--- components/Player/mini-player.tsx
+++ components/Player/mini-player.tsx
@@ -40,7 +40,7 @@ export function Miniplayer({ navigation }: { navigation : NavigationHelpers<Para
                                     flex={1}>
                                         <BlurhashedImage
                                             item={nowPlaying!.item}
-                                            width={getToken("$7")}
+                                            width={getToken("$15")}
                                             borderRadius={2}
                                             />
 
