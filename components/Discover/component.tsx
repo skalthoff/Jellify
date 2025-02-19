986d8cc360ac4fbd3e153ecb592b1191a24b9378
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-18 22:54:30 -0600

    too much

--- components/Discover/component.tsx
+++ components/Discover/component.tsx
@@ -5,7 +5,6 @@ import RecentlyAdded from "./helpers/just-added";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { StackParamList } from "../types";
 import { H2 } from "../Global/helpers/text";
-import Client from "../../api/client";
 
 export default function Index({ navigation }: { navigation : NativeStackNavigationProp<StackParamList> }) : React.JSX.Element {
     return (
@@ -14,7 +13,7 @@ export default function Index({ navigation }: { navigation : NativeStackNavigati
                 contentInsetAdjustmentBehavior="automatic"
                 removeClippedSubviews
             >
-                <H2>{`Recently added to ${Client.server?.name ?? "Jellyfin"}`}</H2>
+                <H2>{`Recently added`}</H2>
                 <RecentlyAdded navigation={navigation} />
             </ScrollView>
         </SafeAreaView>
