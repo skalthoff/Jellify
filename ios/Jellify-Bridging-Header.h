9bd3f696f843ef4bbe6b2375fa9478e4c28a863a
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-22 17:44:16 -0600

    fix margins

--- ios/Jellify-Bridging-Header.h
+++ ios/Jellify-Bridging-Header.h
@@ -1,14 +1,3 @@
 #import "RNCarPlay.h"
 #import <Expo/Expo.h>
 #import <RCTAppDelegate.h>
-
-#ifdef DEBUG
-#ifdef FB_SONARKIT_ENABLED
-#import <FlipperKit/FlipperClient.h>
-#import <FlipperKitLayoutPlugin/FlipperKitLayoutPlugin.h>
-#import <FlipperKitUserDefaultsPlugin/FKUserDefaultsPlugin.h>
-#import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
-#import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
-#import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
-#endif
-#endif
