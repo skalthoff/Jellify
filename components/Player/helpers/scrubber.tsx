2fac2949a90c59d5ef63d05fac8cb7e47e0f21fc
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-22 17:43:45 -0600

    okay??

--- components/Player/helpers/scrubber.tsx
+++ components/Player/helpers/scrubber.tsx
@@ -83,15 +83,15 @@ export default function Scrubber() : React.JSX.Element {
             </GestureDetector>
 
             <XStack marginTop={"$3"} marginBottom={"$2"}>
-                <YStack flex={1} alignContent="flex-start">
+                <YStack flex={1} alignItems="flex-start">
                     <RunTimeSeconds>{Math.floor(position / ProgressMultiplier)}</RunTimeSeconds>
                 </YStack>
 
-                <YStack flex={1} alignContent="center">
+                <YStack flex={1} alignItems="center">
                     { /** Track metadata can go here */}
                 </YStack>
 
-                <YStack flex={1} alignContent="flex-end">
+                <YStack flex={1} alignItems="flex-end">
                     <RunTimeSeconds>
                         {
                             progress && progress.duration
