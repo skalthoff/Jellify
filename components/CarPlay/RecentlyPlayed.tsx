ed86e0e0bb5377522c38da54178d8400b7a8071b
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-23 10:56:51 -0600

    fix recently played

--- components/CarPlay/RecentlyPlayed.tsx
+++ components/CarPlay/RecentlyPlayed.tsx
@@ -3,13 +3,19 @@ import { ListTemplate } from "react-native-carplay";
 
 export const CarPlayRecentlyPlayed = (recentTracks : BaseItemDto[]) => new ListTemplate({
     title: "Recently Played",
-    items: recentTracks.map(track => {
-        return {
-            id: track.Id!,
-            text: track.Name ? track.Name : "Untitled Track",
-            // image: {
-            //     uri: `file://${getImageFilePath(track.Id!, 150, 150, ImageType.Primary)}`
-            // }
+    
+    sections: [
+        {
+            header: `Recently Played`,
+            items: recentTracks.map(track => {
+                return {
+                    id: track.Id!,
+                    text: track.Name ? track.Name : "Untitled Track",
+                    // image: {
+                    //     uri: `file://${getImageFilePath(track.Id!, 150, 150, ImageType.Primary)}`
+                    // }
+                }
+            })
         }
-    })     
+    ]     
 })
