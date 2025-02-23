529606ce0c724598f2cb8453dfc444a230331fa0
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-23 11:38:27 -0600

    fix templates getting pushed multiple times

--- components/CarPlay/ListTemplate.tsx
+++ components/CarPlay/ListTemplate.tsx
@@ -1,17 +1,17 @@
 import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import { ListTemplate } from "react-native-carplay";
 
-const ListItemTemplate = (items: BaseItemDto[] | undefined) => new ListTemplate({
-    sections: [
-        {
-            items: items?.map(item => {
-                return {
-                    id: item.Id!,
-                    text: item.Name ?? "Untitled"
-                }
-            }) ?? []
-        }
-    ]
-})
-
-export default ListItemTemplate;
+export default function ListItemTemplate(items: BaseItemDto[] | undefined) : ListTemplate {
+    return new ListTemplate({
+        sections: [
+            {
+                items: items?.map(item => {
+                    return {
+                        id: item.Id!,
+                        text: item.Name ?? "Untitled"
+                    }
+                }) ?? []
+            }
+        ]
+    })
+}
