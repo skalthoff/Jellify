3d9d9cfa373a72b9d84238b29942a863a31564ca
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-23 11:04:02 -0600

    does this work for images?

--- components/CarPlay/RecentArtists.tsx
+++ components/CarPlay/RecentArtists.tsx
@@ -1,7 +1,9 @@
+import { queryClient } from "@/constants/query-client"
 import Client from "../../api/client"
-import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models"
+import { BaseItemDto, ImageType } from "@jellyfin/sdk/lib/generated-client/models"
 import { getImageApi } from "@jellyfin/sdk/lib/utils/api"
 import { GridTemplate, ListTemplate } from "react-native-carplay"
+import { QueryKeys } from "@/enums/query-keys"
 
 export const CarPlayRecentArtists = (artists : BaseItemDto[]) => new ListTemplate({
     title: "Recently Played",
@@ -12,7 +14,7 @@ export const CarPlayRecentArtists = (artists : BaseItemDto[]) => new ListTemplat
                     id: artist.Id!,
                     text: artist.Name ? artist.Name : "Untitled Track",
                     image: {
-                        uri: getImageApi(Client.api!).getItemImageUrlById(artist.Id!)
+                        uri: queryClient.getQueryData([ QueryKeys.ItemImage, ImageType.Primary ])
                     }
                 }
             })
