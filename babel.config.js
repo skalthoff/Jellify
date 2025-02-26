f094d2edfb167a71fdba492292f5a13d652faf40
Violet Caulfield <violet@cosmonautical.cloud>
2025-02-26 02:23:47 -0600

    yolo

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-
+    'react-native-boost/plugin',
     // react-native-reanimated/plugin has to be listed last
     'react-native-reanimated/plugin',
   ]
