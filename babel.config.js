f6b3549d7ad9bcb582866f87e875947ced875224
Violet Caulfield <violet@cosmonautical.cloud>
2025-03-23 07:30:05 -0500

    fix build?

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,6 @@ module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
     'react-native-boost/plugin',
-    {
-      ignores: ['node_modules/**'],
-    },
     // react-native-reanimated/plugin has to be listed last
     'react-native-reanimated/plugin',
   ]
--- components/CarPlay/ListTemplate.tsx
+++ components/CarPlay/ListTemplate.tsx
@@ -1,3 +1,5 @@
+import { queryClient } from "../../constants/query-client";
+import { QueryKeys } from "../../enums/query-keys";
 import { BaseItemDto } from "@jellyfin/sdk/lib/generated-client/models";
 import { ListTemplate } from "react-native-carplay";
 
@@ -8,10 +10,13 @@ export default function ListItemTemplate(items: BaseItemDto[] | undefined) : Lis
                 items: items?.map(item => {
                     return {
                         id: item.Id!,
-                        text: item.Name ?? "Untitled"
+                        text: item.Name ?? "Untitled",
+                        image: {
+                            uri: queryClient.getQueryData<string | undefined>([QueryKeys.ItemImage, item.Id!])
+                        }
                     }
                 }) ?? []
             }
-        ]
+        ],
     })
 }
