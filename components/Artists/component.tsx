8ad410a82859775856dfe8cbdcbad45350459017
John Grant <john@johngrant.dev>
2025-04-02 10:48:26 +1100

    Handle all artists

--- components/Artists/component.tsx
+++ components/Artists/component.tsx
@@ -5,6 +5,7 @@ import { ItemCard } from "../Global/components/item-card";
 import { ArtistsProps } from "../types";
 import { QueryKeys } from "../../enums/query-keys";
 import { useQuery } from "@tanstack/react-query";
+import { fetchArtists } from "../../api/queries/functions/artists";
 import { fetchRecentlyPlayedArtists } from "../../api/queries/functions/recents";
 import { fetchFavoriteArtists } from "../../api/queries/functions/favorites";
 import { QueryConfig } from "../../api/queries/query.config";
@@ -19,12 +20,17 @@ export default function Artists({
             QueryKeys.RecentlyPlayedArtists ? useQuery({
                 queryKey: [QueryKeys.RecentlyPlayedArtists, QueryConfig.limits.recents * 4, QueryConfig.limits.recents],
                 queryFn: () => fetchRecentlyPlayedArtists(QueryConfig.limits.recents * 4, QueryConfig.limits.recents)
-            }) :
-            
+            }) : 
+            route.params.query === 
+            QueryKeys.FavoriteArtists ? 
             useQuery({
                 queryKey: [QueryKeys.FavoriteArtists],
                 queryFn: () => fetchFavoriteArtists()
-            });
+            }) :
+            useQuery({
+                queryKey: [QueryKeys.AllArtists],
+                queryFn: () => fetchArtists()
+            })
 
     const { width } = useSafeAreaFrame();
 
