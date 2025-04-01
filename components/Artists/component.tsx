441506615db6a5b45d8a9895ad8d3cbdb74ee109
John Grant <john@johngrant.dev>
2025-04-02 10:48:26 +1100

    Handle all artists

--- components/Artists/component.tsx
+++ components/Artists/component.tsx
@@ -1,36 +1,38 @@
-import { useSafeAreaFrame } from 'react-native-safe-area-context'
-import React from 'react'
-import { FlatList, RefreshControl } from 'react-native'
-import { ItemCard } from '../Global/components/item-card'
-import { ArtistsProps } from '../types'
-import { QueryKeys } from '../../enums/query-keys'
-import { useQuery } from '@tanstack/react-query'
-import { fetchRecentlyPlayedArtists } from '../../api/queries/functions/recents'
-import { fetchFavoriteArtists } from '../../api/queries/functions/favorites'
-import { QueryConfig } from '../../api/queries/query.config'
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+import React from "react";
+import { FlatList, RefreshControl } from "react-native";
+import { ItemCard } from "../Global/components/item-card";
+import { ArtistsProps } from "../types";
+import { QueryKeys } from "../../enums/query-keys";
+import { useQuery } from "@tanstack/react-query";
+import { fetchArtists } from "../../api/queries/functions/artists";
+import { fetchRecentlyPlayedArtists } from "../../api/queries/functions/recents";
+import { fetchFavoriteArtists } from "../../api/queries/functions/favorites";
+import { QueryConfig } from "../../api/queries/query.config";
 
-export default function Artists({ navigation, route }: ArtistsProps): React.JSX.Element {
-	const {
-		data: artists,
-		refetch,
-		isPending,
-	} = route.params.query === QueryKeys.RecentlyPlayedArtists
-		? useQuery({
-				queryKey: [
-					QueryKeys.RecentlyPlayedArtists,
-					QueryConfig.limits.recents * 4,
-					QueryConfig.limits.recents,
-				],
-				queryFn: () =>
-					fetchRecentlyPlayedArtists(
-						QueryConfig.limits.recents * 4,
-						QueryConfig.limits.recents,
-					),
-		  })
-		: useQuery({
-				queryKey: [QueryKeys.FavoriteArtists],
-				queryFn: () => fetchFavoriteArtists(),
-		  })
+export default function Artists({ 
+    navigation,
+    route
+}: ArtistsProps): React.JSX.Element {
+
+    const { data: artists, refetch, isPending } = 
+        route.params.query === 
+            QueryKeys.RecentlyPlayedArtists ? useQuery({
+                queryKey: [QueryKeys.RecentlyPlayedArtists, QueryConfig.limits.recents * 4, QueryConfig.limits.recents],
+                queryFn: () => fetchRecentlyPlayedArtists(QueryConfig.limits.recents * 4, QueryConfig.limits.recents)
+            }) : 
+            route.params.query === 
+            QueryKeys.FavoriteArtists ? 
+            useQuery({
+                queryKey: [QueryKeys.FavoriteArtists],
+                queryFn: () => fetchFavoriteArtists()
+            }) :
+            useQuery({
+                queryKey: [QueryKeys.AllArtists],
+                queryFn: () => fetchArtists()
+            })
+
+    const { width } = useSafeAreaFrame();
 
 	return (
 		<FlatList
