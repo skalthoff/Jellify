6d6c54d699e26f8e286eb005ccc47e4107becd19
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-04-03 07:30:31 -0700

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
+// https://docs.swmansion.com/react-native-gesture-handler/docs/guides/testing
 module.exports = {
   preset: 'react-native',
+  setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"]
 };
