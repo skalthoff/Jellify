81624c959a20f46c44ac9409d6351c4857aff546
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-04-03 08:56:56 -0700

    update jest.config.js
    Changes taken from @charpeni :pray:

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,8 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
-  extensionsToTreatAsEsm: ['.ts', '.tsx']
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  transformIgnorePatterns: [
+'node_modules/(?!((@)?react-native|react-native-url-polyfill|@react-navigation|react-native-track-player)/)',
+  ],
 };
