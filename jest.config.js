12f9e92b001e2efd03d04b79ae93015408f1bdcf
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-04-03 09:06:31 -0700

    jest configuration
    Create jest folder, create setup.js file used after env

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'react-native',
   setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
+  setupFilesAfterEnv:  ["./jest/setup.js"],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transformIgnorePatterns: [
 'node_modules/(?!((@)?react-native|react-native-url-polyfill|@react-navigation|react-native-track-player)/)',
