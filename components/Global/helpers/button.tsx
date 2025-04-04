6305e1c82b02cea85a43413f1a8006f824eb9766
John Grant <john@johngrant.dev>
2025-04-05 10:01:29 +1100

    added focus for button highlight

--- components/Global/helpers/button.tsx
+++ components/Global/helpers/button.tsx
@@ -1,14 +1,27 @@
-import { Button as TamaguiButton, ButtonProps as TamaguiButtonProps } from 'tamagui'
+import { Button as TamaguiButton } from 'tamagui';
 
-interface ButtonProps extends TamaguiButtonProps {
-	children?: Element | string | undefined
-	onPress?: () => void | undefined
-	disabled?: boolean | undefined
-	danger?: boolean | undefined
+// Hardcoded theme for focus - not ideal
+
+interface ButtonProps {
+    children?: Element | string | undefined;
+    onPress?: () => void | undefined;
+    disabled?: boolean | undefined;
+    danger?: boolean | undefined;
+    focus?: boolean | undefined;
 }
 
 export default function Button(props: ButtonProps): React.JSX.Element {
-	return (
-		<TamaguiButton bordered opacity={props.disabled ? 0.5 : 1} marginVertical={30} {...props} />
-	)
+
+    return (
+        <TamaguiButton 
+            disabled={props.disabled}
+            opacity={props.disabled ? 0.5 : 1}
+            bordered
+            onPress={props.onPress}
+            theme={props.focus ? "dark_inverted_purple" : undefined}
+            {...props}
+        >
+            { props.children }
+        </TamaguiButton>
+    )
 }
