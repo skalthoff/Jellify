8575fab36482bf3954113a8178a8ccf85af3af4c
John Grant <john@johngrant.dev>
2025-04-04 22:05:17 +1100

    Refactored from favorites page to Library Header Component

--- components/Library/component.tsx
+++ components/Library/component.tsx
@@ -1,36 +1,54 @@
-import { FlatList } from 'react-native'
-import { useSafeAreaFrame } from 'react-native-safe-area-context'
-import Categories from './categories'
-import IconCard from '../../components/Global/helpers/icon-card'
-import { StackParamList } from '../../components/types'
-import { RouteProp } from '@react-navigation/native'
-import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+import Categories from "./categories";
+import { StackParamList } from "../../components/types";
+import { RouteProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ScrollView, View } from "tamagui";
+import Button from "../Global/helpers/button";
+import { StyleSheet } from "react-native";
 
-export default function Library({
-	route,
-	navigation,
-}: {
-	route: RouteProp<StackParamList, 'Library'>
-	navigation: NativeStackNavigationProp<StackParamList>
+export default function LibraryTopBar({ 
+    route, 
+    navigation 
+} : {
+    route: RouteProp<StackParamList, keyof StackParamList>,
+    navigation: NativeStackNavigationProp<StackParamList, keyof StackParamList>;
 }): React.JSX.Element {
-	const { width } = useSafeAreaFrame()
 
-	return (
-		<FlatList
-			contentInsetAdjustmentBehavior='automatic'
-			data={Categories}
-			numColumns={2}
-			renderItem={({ index, item }) => (
-				<IconCard
-					name={item.iconName}
-					caption={item.name}
-					width={width / 2.1}
-					onPress={() => {
-						navigation.navigate(item.name, item.params)
-					}}
-					largeIcon
-				/>
-			)}
-		/>
-	)
+    const { width } = useSafeAreaFrame();
+
+    // janky fix to hardcoded margin on react navigation parent container see:
+    // https://github.com/react-navigation/react-navigation/blob/d814a40e3e632ca67b1e92ff4f3086a80638cc1a/packages/elements/src/Header/HeaderBackButton.tsx#L192C8-L192C8
+    // issue found on android, need to test to see if this is an issue on ios
+    const styles = StyleSheet.create({
+        headerFix: {
+            marginHorizontal: -11, 
+            paddingLeft: 11,
+        },
+        spacer: {
+            width: 11
+        }
+    })
+
+    return (
+        <ScrollView 
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+                gap: 6
+            }}
+            style={styles.headerFix}
+        >
+            {
+                Categories.map((item, index) => {
+                    return <>
+                        <Button key={index} focus={route.name.toLowerCase() === item.name.toLowerCase() ? true : false}>
+                            {item.name}
+                        </Button>
+                    </>
+                })
+            }
+            <View style={styles.spacer} />
+        </ScrollView>
+    )
 }
