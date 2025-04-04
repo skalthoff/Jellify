e8bd7dffd7c027366b791e649468225d825a4188
John Grant <john@johngrant.dev>
2025-04-04 22:26:03 +1100

    moved favorites from components/library to components/favorites

---
+++ components/Favorites/component.tsx
@@ -0,0 +1,40 @@
+import { FlatList, Text } from "react-native";
+import { useSafeAreaFrame } from "react-native-safe-area-context";
+import IconCard from "../../components/Global/helpers/icon-card";
+import { StackParamList } from "../../components/types";
+import { RouteProp } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { ScrollView, View } from "tamagui";
+import Button from "../Global/helpers/button";
+import Categories from "./categories";
+import LibraryTopBar from "../Library/component";
+
+export default function Favorites({ 
+    route, 
+    navigation 
+} : {
+    route: RouteProp<StackParamList, "Favorites">,
+    navigation: NativeStackNavigationProp<StackParamList>
+}): React.JSX.Element {
+
+    const { width } = useSafeAreaFrame();
+
+    return (
+        <FlatList
+            contentInsetAdjustmentBehavior="automatic"
+            data={Categories}
+            numColumns={2}
+            renderItem={({ index, item }) =>
+                    <IconCard 
+                        name={item.iconName}
+                        caption={`Favorite ${item.name}`}
+                        width={width / 2.1}
+                        onPress={() => {
+                            navigation.navigate(item.name, item.params)
+                        }}
+                        largeIcon
+                    />
+            }
+        />
+    )
+}
