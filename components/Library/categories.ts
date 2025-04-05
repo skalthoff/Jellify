9a43365bc604b1c0f100a3026c4ab3cbd1344ab5
John Grant <john@johngrant.dev>
2025-04-06 09:55:30 +1000

    Refactored Favorites categories for header buttons

--- components/Library/categories.ts
+++ components/Library/categories.ts
@@ -1,18 +1,18 @@
 import { QueryKeys } from '../../enums/query-keys'
 
 interface CategoryRoute {
-	name: any // ¯\_(ツ)_/¯
-	iconName: string
-	params?: {
-		query: QueryKeys
-	}
-}
+    name: any; // ¯\_(ツ)_/¯
+    params?: {
+        query: QueryKeys
+    };
+};
 
 const Categories : CategoryRoute[] = [
-    { name: "Artists", iconName: "microphone-variant", params: { query: QueryKeys.AllArtists } },
-    { name: "Albums", iconName: "music-box-multiple", params: { query: QueryKeys.AllAlbums} },
-    { name: "Favorite Tracks", iconName: "music-note", params: { query: QueryKeys.FavoriteTracks } },
-    { name: "Playlists", iconName: "playlist-music" },
+    { name: "Favorites", params: { query: QueryKeys.Favorites } },
+    { name: "Artists", params: { query: QueryKeys.AllArtists } },
+    { name: "Albums", params: { query: QueryKeys.AllAlbums} },
+    { name: "Genres", params: { query: QueryKeys.Genres} },
+    { name: "Playlists", params: { query: QueryKeys.Playlists}  },
 ];
 
 export default Categories
