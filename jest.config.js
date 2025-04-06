e984cd765dc891bcdbc117ac26346f3de1a83d37
Violet Caulfield <violet@cosmonautical.cloud>
2025-04-06 10:46:27 -0500

    fix tamagui token conf

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   preset: 'jest-expo',
   setupFiles: ["./node_modules/react-native-gesture-handler/jestSetup.js"],
-  setupFilesAfterEnv:  ["./jest/setup.js"],
+  setupFilesAfterEnv:  [
+    "./jest/setup.js",
+    "./tamagui.config.ts",
+  ],
   extensionsToTreatAsEsm: ['.ts', '.tsx'],
   transformIgnorePatterns: [
     'node_modules/(?!(@)?(react-native|react-native-.*|react-navigation|jellyfin|burnt|expo|expo-.*)/)',
