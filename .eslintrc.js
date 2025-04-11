e63c63bd5e45d5f7be2ecc053e3c691846be73f1
Violet Caulfield <violet@cosmonautical.cloud>
2025-04-11 07:05:30 -0500

    update eslint, incorporate husky hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,21 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'react'],
+	env: {
+		browser: true,
+		node: true,
+	},
+	rules: {
+		'react/react-in-jsx-scope': 'off',
+		'no-unused-vars': 'warn',
+		'react/prop-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'error', // Disallow usage of any
+		'@typescript-eslint/explicit-module-boundary-types': 'error', // Ensure types are explicitly declared
+	},
 };
