a12fcc4855cabf680395397cc84f5737b5bd4417
Violet Caulfield <violet@cosmonautical.cloud>
2025-04-13 10:00:20 -0500

    Add Images to CarPlay list templates
    bump some packages

--- components/CarPlay/ListTemplate.tsx
+++ components/CarPlay/ListTemplate.tsx
@@ -1,26 +1,57 @@
-import { queryClient } from '../../constants/query-client'
-import { QueryKeys } from '../../enums/query-keys'
+import { Image } from 'expo-image'
 import { BaseItemDto } from '@jellyfin/sdk/lib/generated-client/models'
 import { ListTemplate } from 'react-native-carplay'
+import { getImageApi } from '@jellyfin/sdk/lib/utils/api'
+import Client from '../../api/client'
 
-export default function ListItemTemplate(items: BaseItemDto[] | undefined): ListTemplate {
-	return new ListTemplate({
-		sections: [
-			{
-				items:
-					items?.map((item) => {
+interface ListItemImage {
+	id: string
+	url: string | null
+}
+
+export default async function ListItemTemplate(
+	items: BaseItemDto[] | undefined,
+): Promise<ListTemplate> {
+	if (items) {
+		const itemImages: ListItemImage[] = await Promise.all(
+			items.map(async (item): Promise<ListItemImage> => {
+				return Image.getCachePathAsync(
+					getImageApi(Client.api!).getItemImageUrlById(item.Id!),
+				).then((imageUri) => {
+					if (imageUri)
 						return {
 							id: item.Id!,
-							text: item.Name ?? 'Untitled',
-							image: {
-								uri: queryClient.getQueryData<string | undefined>([
-									QueryKeys.ItemImage,
-									item.Id!,
-								]),
-							},
+							url: imageUri,
+						} as ListItemImage
+					else {
+						return {
+							id: item.Id!,
+							url: getImageApi(Client.api!).getItemImageUrlById(item.Id!),
 						}
-					}) ?? [],
-			},
-		],
-	})
+					}
+				})
+			}),
+		)
+
+		return new ListTemplate({
+			sections: [
+				{
+					items:
+						items?.map((item) => {
+							return {
+								id: item.Id!,
+								text: item.Name ?? 'Untitled',
+								image: {
+									uri:
+										itemImages.find((itemImage) => itemImage.id === item.Id)!
+											.url ?? undefined,
+								},
+							}
+						}) ?? [],
+				},
+			],
+		})
+	} else {
+		return new ListTemplate({})
+	}
 }
