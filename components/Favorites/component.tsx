f7c17066b588bc02f9891146659a666afbb4bfbe
John Grant <john@johngrant.dev>
2025-04-13 21:13:21 +1000

    cleaned up , added padding

--- components/Favorites/component.tsx
+++ components/Favorites/component.tsx
@@ -1,13 +1,10 @@
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, View } from "react-native";
 import { useSafeAreaFrame } from "react-native-safe-area-context";
 import IconCard from "../../components/Global/helpers/icon-card";
 import { StackParamList } from "../../components/types";
 import { RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { ScrollView, View } from "tamagui";
-import Button from "../Global/helpers/button";
 import Categories from "./categories";
-import LibraryTopBar from "../Library/component";
 import { useHeaderHeight } from "@react-navigation/elements";
 
 export default function Favorites({ 
@@ -15,29 +12,30 @@ export default function Favorites({
     navigation 
 } : {
     route: RouteProp<StackParamList, "Favorites">,
-    navigation: NativeStackNavigationProp<StackParamList>
+    navigation: NativeStackNavigationProp<StackParamList, keyof StackParamList>
 }): React.JSX.Element {
 
     const { width } = useSafeAreaFrame();
     const headerHeight = useHeaderHeight();
 
     return (
-        <FlatList
-            style={{ paddingTop: headerHeight }}
-            contentInsetAdjustmentBehavior="automatic"
-            data={Categories}
-            numColumns={2}
-            renderItem={({ index, item }) =>
-                    <IconCard 
-                        name={item.iconName}
-                        caption={`Favorite ${item.name}`}
-                        width={width / 2.1}
-                        onPress={() => {
-                            navigation.navigate(item.name, item.params)
-                        }}
-                        largeIcon
-                    />
-            }
-        />
+        <View style={{ paddingTop: headerHeight }} >
+            <FlatList
+                contentInsetAdjustmentBehavior="automatic"
+                data={Categories}
+                numColumns={2}
+                renderItem={({ index, item }) =>
+                        <IconCard 
+                            name={item.iconName}
+                            caption={`${item.name}`}
+                            width={width / 2.1}
+                            onPress={() => {
+                                navigation.navigate(item.name, item.params)
+                            }}
+                            largeIcon
+                        />
+                }
+            />
+        </View>
     )
 }
