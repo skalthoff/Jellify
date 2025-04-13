7693f0647b7d8535ebf12e2283da9e5e93e5fb91
John Grant <john@johngrant.dev>
2025-04-13 21:12:37 +1000

    Service logic moved to Library component

--- components/Albums/component.tsx
+++ components/Albums/component.tsx
@@ -1,55 +1,31 @@
+import React from "react";
 import { AlbumsProps } from "../types";
-import { useSafeAreaFrame } from "react-native-safe-area-context";
-import { ItemCard } from "../Global/components/item-card";
-import { FlatList, RefreshControl } from "react-native";
-import { useQuery } from "@tanstack/react-query";
-import { QueryKeys } from "../../enums/query-keys";
-import { fetchFavoriteAlbums } from "../../api/queries/functions/favorites";
-import { fetchRecentlyAdded } from "../../api/queries/functions/recents";
-import { QueryConfig } from "../../api/queries/query.config";
-import { fetchAlbums } from "../../api/queries/functions/albums";
-import { useHeaderHeight } from "@react-navigation/elements";
+import { BaseItemKind } from "@jellyfin/sdk/lib/generated-client/models";
+import Library from "../Library/component";
 
-export default function Albums({ navigation, route }: AlbumsProps) : React.JSX.Element {
-
-    const { data: albums, refetch, isPending } = 
-        useQuery({
-            queryKey: [route.params.query],
-            queryFn: () => 
-                QueryKeys.RecentlyAdded 
-                ? fetchRecentlyAdded(QueryConfig.limits.recents * 4, QueryConfig.limits.recents) 
-                : QueryKeys.FavoriteAlbums ?
-                    fetchFavoriteAlbums()
-                    : fetchAlbums()
-        });
-
-    const { width } = useSafeAreaFrame();
-    const headerHeight = useHeaderHeight();
-
-        return (
-            <FlatList
-                style={{ paddingTop: headerHeight }}
-                contentInsetAdjustmentBehavior="automatic"
-                numColumns={2}
-                data={albums}
-                refreshControl={
-                    <RefreshControl
-                        refreshing={isPending}
-                        onRefresh={refetch}
-                    />
+export default function Albums({ 
+    navigation,
+    route
+}: AlbumsProps): React.JSX.Element {
+    return (
+        <Library
+            navigation={navigation}
+            route={{
+                ...route,
+                params: {
+                    ...route.params,
+                    itemType: BaseItemKind.MusicAlbum,
+                    navigationParams: {
+                        screen: "Album",
+                        params: (item) => ({ album: item })
+                    }
                 }
-                renderItem={({ index, item: album}) => 
-                    <ItemCard
-                        item={album}
-                        caption={album.Name ?? "Untitled Album"}
-                        subCaption={album.ProductionYear?.toString() ?? ""}
-                        squared
-                        onPress={() => {
-                            navigation.navigate("Album", { album })
-                        }}
-                        width={width / 2.1}
-                    />   
-                }
-            />
-        )
-    }
+            }}
+            displayStyle={{
+                numColumns: 3,
+                squared: true,
+                layout: 'grid'
+            }}
+        />
+    );
+}
--- components/Artists/component.tsx
+++ components/Artists/component.tsx
@@ -1,69 +1,32 @@
-import { useSafeAreaFrame } from "react-native-safe-area-context";
 import React from "react";
-import { FlatList, RefreshControl } from "react-native";
-import { ItemCard } from "../Global/components/item-card";
 import { ArtistsProps } from "../types";
 import { QueryKeys } from "../../enums/query-keys";
-import { useQuery } from "@tanstack/react-query";
-import { fetchArtists } from "../../api/queries/functions/artists";
-import { fetchRecentlyPlayedArtists } from "../../api/queries/functions/recents";
-import { fetchFavoriteArtists } from "../../api/queries/functions/favorites";
-import { QueryConfig } from "../../api/queries/query.config";
-import { useHeaderHeight } from '@react-navigation/elements';
-
-const queryMap = {
-    [QueryKeys.RecentlyPlayedArtists] : {
-        key: [QueryKeys.RecentlyPlayedArtists, QueryConfig.limits.recents * 4, QueryConfig.limits.recents],
-        fn: () => fetchRecentlyPlayedArtists(QueryConfig.limits.recents * 4, QueryConfig.limits.recents)
-    },
-    [QueryKeys.FavoriteArtists] : {
-        key: [QueryKeys.FavoriteArtists],
-        fn: () => fetchFavoriteArtists()
-    },
-    [QueryKeys.AllArtists] : {
-        key: [QueryKeys.AllArtists],
-        fn: () => fetchArtists()
-    }
-}
+import { BaseItemKind } from "@jellyfin/sdk/lib/generated-client/models";
+import Library from "../Library/component";
 
 export default function Artists({ 
     navigation,
     route
 }: ArtistsProps): React.JSX.Element {
-
-    const queryType = queryMap[route.params.query] ?? queryMap[QueryKeys.AllArtists]
- 
-    const { data: artists, refetch, isPending } = useQuery({
-                 queryKey: queryType.key,
-                 queryFn: queryType.fn,
-            })
-
-    const { width } = useSafeAreaFrame();
-    const headerHeight = useHeaderHeight();
-
-
     return (
-        <FlatList
-            style={{ paddingTop: headerHeight }}
-            contentInsetAdjustmentBehavior="automatic"
-            numColumns={3}
-            data={artists}
-            refreshControl={
-                <RefreshControl
-                    refreshing={isPending}
-                    onRefresh={refetch}
-                />
-            }
-            renderItem={({ index, item: artist}) =>
-                <ItemCard
-                    item={artist}
-                    caption={artist.Name ?? "Unknown Artist"}
-                    onPress={() => {
-                        navigation.navigate("Artist", { artist })
-                    }}
-                    width={width / 3.3}
-                />
-            }
+        <Library
+            navigation={navigation}
+            route={{
+                ...route,
+                params: {
+                    ...route.params,
+                    itemType: BaseItemKind.MusicArtist,
+                    navigationParams: {
+                        screen: "Artist",
+                        params: (item) => ({ artist: item })
+                    }
+                }
+            }}
+            displayStyle={{
+                numColumns: 3,
+                squared: true,
+                layout: 'grid'
+            }}
         />
     )
 }
