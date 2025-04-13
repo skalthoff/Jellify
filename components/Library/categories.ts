c5e862c7ea14ddfc7d566a4cb86275721e78ea95
John Grant <john@johngrant.dev>
2025-04-13 21:13:55 +1000

    added tracks to header disabled genres for now

--- components/Library/categories.ts
+++ components/Library/categories.ts
@@ -11,7 +11,8 @@ const Categories : CategoryRoute[] = [
     { name: "Favorites", params: { query: QueryKeys.Favorites } },
     { name: "Artists", params: { query: QueryKeys.AllArtists } },
     { name: "Albums", params: { query: QueryKeys.AllAlbums} },
-    { name: "Genres", params: { query: QueryKeys.Genres} },
+    { name: "Tracks", params: { query: QueryKeys.AllAlbums} },
+    // { name: "Genres", params: { query: QueryKeys.Genres} },
     { name: "Playlists", params: { query: QueryKeys.Playlists}  },
 ];
 
