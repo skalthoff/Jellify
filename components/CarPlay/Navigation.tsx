09bba48f7f9523711165cf15f3f03e18dcddb64f
Violet Caulfield <violet@cosmonautical.cloud>
2025-04-14 17:41:27 -0500

    carplay adjustments on how tabs work

--- components/CarPlay/Navigation.tsx
+++ components/CarPlay/Navigation.tsx
@@ -3,13 +3,12 @@ import CarPlayHome from './Home'
 import CarPlayDiscover from './Discover'
 import uuid from 'react-native-uuid'
 
-const CarPlayNavigation: TabBarTemplate = new TabBarTemplate({
-	id: uuid.v4(),
-	title: 'Tabs',
-	templates: [CarPlayHome(), CarPlayDiscover()],
-	onTemplateSelect(template, e) {
-		if (template) CarPlay.pushTemplate(template, true)
-	},
-})
+const CarPlayNavigation = () =>
+	new TabBarTemplate({
+		id: uuid.v4(),
+		title: 'Tabs',
+		templates: [CarPlayHome(), CarPlayDiscover()],
+		onTemplateSelect(template, e) {},
+	})
 
 export default CarPlayNavigation
--- components/provider.tsx
+++ components/provider.tsx
@@ -29,7 +29,7 @@ const JellifyContextInitializer = () => {
 
 			if (loggedIn) {
 				console.debug(CarPlay.bridge)
-				CarPlay.setRootTemplate(CarPlayNavigation)
+				CarPlay.setRootTemplate(CarPlayNavigation())
 				CarPlay.pushTemplate(CarPlayNowPlaying())
 
 				if (Platform.OS === 'ios') CarPlay.enableNowPlaying(true) // https://github.com/birkir/react-native-carplay/issues/185
