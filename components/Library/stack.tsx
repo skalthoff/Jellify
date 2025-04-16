7f3847ecde0f9f81f9483a33daa9a8fd72a339f1
Violet Caulfield <violet@cosmonautical.cloud>
2025-04-16 14:36:16 -0500

    make artist headers regular sized on iOS

--- components/Library/stack.tsx
+++ components/Library/stack.tsx
@@ -34,7 +34,6 @@ export default function LibraryStack(): React.JSX.Element {
 				component={ArtistScreen}
 				options={({ route }) => ({
 					title: route.params.artist.Name ?? 'Unknown Artist',
-					headerLargeTitle: true,
 					headerTitleStyle: {
 						fontFamily: 'Aileron-Bold',
 					},
--- components/Search/stack.tsx
+++ components/Search/stack.tsx
@@ -27,8 +27,7 @@ export default function SearchStack(): React.JSX.Element {
 				component={ArtistScreen}
 				options={({ route }) => ({
 					title: route.params.artist.Name ?? 'Unknown Artist',
-					headerLargeTitle: true,
-					headerLargeTitleStyle: {
+					headerTitleStyle: {
 						fontFamily: 'Aileron-Bold',
 					},
 				})}
