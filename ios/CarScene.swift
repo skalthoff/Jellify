2f69d479a9a6a6bc850c6e3b69d226f846e7cbd4
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-04-20 06:46:27 -0500

    set up RN bridge on CarScene

--- ios/CarScene.swift
+++ ios/CarScene.swift
@@ -5,6 +5,18 @@ import CarPlay
 class CarSceneDelegate: UIResponder, CPTemplateApplicationSceneDelegate {
   func templateApplicationScene(_ templateApplicationScene: CPTemplateApplicationScene,
                                   didConnect interfaceController: CPInterfaceController) {
+                                  
+    if let applicationDelegate = UIApplication.shared.delegate as? AppDelegate {
+    if applicationDelegate.bridge == nil {
+        applicationDelegate.bridge = RCTBridge(delegate: applicationDelegate, launchOptions: [:])
+        applicationDelegate.rootView = RCTRootView(
+            bridge: applicationDelegate.bridge,
+            moduleName: "Jellify",
+            initialProperties: nil
+        )
+    }
+}                            
+    
     RNCarPlay.connect(with: interfaceController, window: templateApplicationScene.carWindow);
   }
 
