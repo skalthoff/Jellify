5999bf99a024d7fdd909ad6a93f62d948c2c8242
Violet Caulfield <violet@cosmonautical.cloud>
2025-04-20 23:18:30 -0500

    add some more logging

--- components/auto.tsx
+++ components/auto.tsx
@@ -2,6 +2,8 @@ import { JellyfinAuthenticationProvider } from './Login/provider'
 import Auto from './Auto/component'
 
 export default function JellifyAuto(): React.JSX.Element {
+	console.debug('Launching Auto interface')
+
 	return (
 		<JellyfinAuthenticationProvider>
 			<Auto />
