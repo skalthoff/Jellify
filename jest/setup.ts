fb9e9f06b5b48a9eae7d4c26782fbf68e53a0bf4
Violet Caulfield <violet@cosmonautical.cloud>
2025-05-02 08:22:01 -0500

    update tests

--- jest/setup.ts
+++ jest/setup.ts
@@ -1,3 +1,19 @@
+jest.mock('../src/api/info', () => {
+	return {
+		JellyfinInfo: {
+			clientInfo: {
+				name: 'Jellify',
+				version: '0.0.1',
+			},
+			deviceInfo: {
+				name: 'iPhone 12',
+				id: '1234567890',
+			},
+			createApi: jest.fn(),
+		},
+	}
+})
+
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter')
 
 jest.mock('react-native-haptic-feedback', () => {
