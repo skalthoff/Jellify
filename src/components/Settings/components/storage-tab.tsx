cbdb979afe846a64d6112c1c45a1613ddc5e9173
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-05-13 15:36:16 -0500

    Move Storage Usage to Storage Tab (#357)
    Move Storage Usage metrics to Storage Tab in Settings
    
    Future updates will include the ability to bulk delete downloads

--- src/components/Settings/components/storage-tab.tsx
+++ src/components/Settings/components/storage-tab.tsx
@@ -2,13 +2,24 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import SettingsListGroup from './settings-list-group'
 import { SwitchWithLabel } from '../../Global/helpers/switch-with-label'
 import { useSettingsContext } from '../../../providers/Settings'
+import { useNetworkContext } from '../../../providers/Network'
 
 export default function StorageTab(): React.JSX.Element {
 	const { autoDownload, setAutoDownload } = useSettingsContext()
+	const { downloadedTracks, storageUsage } = useNetworkContext()
+
 	return (
 		<SafeAreaView>
 			<SettingsListGroup
 				settingsList={[
+					{
+						title: 'Usage',
+						subTitle: `${downloadedTracks?.length ?? '0'} ${
+							downloadedTracks?.length === 1 ? 'song' : 'songs'
+						} in your pocket`,
+						iconName: 'harddisk',
+						iconColor: '$borderColor',
+					},
 					{
 						title: 'Automatically Cache Tracks',
 						subTitle: 'Download tracks as they are played',
