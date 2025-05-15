de0f62988cd3891341e521815be23b2b4a8e7159
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-05-15 07:14:52 -0500

    Update Info Tab with Links! (#366)
    Update About Tab in Setting with a plethora of links, including
    
    - Source Code
    - Discord Server
    - GitHub Issues for bug reporting
    - GitHub Sponsors and Patreon

--- src/components/Settings/components/info/index.tsx
+++ src/components/Settings/components/info/index.tsx
@@ -1,13 +1,15 @@
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { version } from '../../../../../package.json'
-import { Text } from '../../../Global/helpers/text'
+import { H5, Text } from '../../../Global/helpers/text'
 import SettingsListGroup from '../settings-list-group'
 import { InfoTabStackNavigationProp } from './types'
 import { useQuery } from '@tanstack/react-query'
 import { QueryKeys } from '../../../../enums/query-keys'
 import { useJellifyContext } from '../../../../providers'
 import fetchPatrons from '../../../../api/queries/patrons'
-import { FlatList } from 'react-native'
+import { FlatList, Linking } from 'react-native'
+import { H6, Separator, XStack, YStack } from 'tamagui'
+import Icon from '../../../Global/components/icon'
 
 export default function InfoTabIndex({ navigation }: InfoTabStackNavigationProp) {
 	const { api } = useJellifyContext()
@@ -26,23 +28,109 @@ export default function InfoTabIndex({ navigation }: InfoTabStackNavigationProp)
 						subTitle: version,
 						iconName: 'jellyfish',
 						iconColor: '$borderColor',
-						children: <Text>Made with 💜 by Violet Caulfield</Text>,
+						children: (
+							<YStack gap={'$2'}>
+								<Text>Made with 💜 by Violet Caulfield</Text>
+
+								<Separator marginBottom={'$2'} />
+								<XStack gap={'$3'}>
+									<XStack
+										alignItems='center'
+										onPress={() =>
+											Linking.openURL('https://github.com/Jellify-Music/App')
+										}
+									>
+										<Icon name='code-tags' small color='$borderColor' />
+										<Text>View Source</Text>
+									</XStack>
+									<XStack
+										alignItems='center'
+										onPress={() =>
+											Linking.openURL('https://discord.gg/yf8fBatktn')
+										}
+									>
+										<Icon name='discord' small color='$borderColor' />
+										<Text>Join Discord</Text>
+									</XStack>
+								</XStack>
+							</YStack>
+						),
 					},
 					{
-						title: 'Sponsored by listeners like you',
-						subTitle:
-							patrons && patrons.length > 1
-								? `${patrons.length.toString()} patrons`
-								: patrons?.length === 1
-									? '1 patron'
-									: '0 patrons',
+						title: 'Caught a bug?',
+						subTitle: "Let's squash it!",
+						iconName: 'bug',
+						iconColor: '$danger',
+						children: (
+							<XStack gap={'$3'} marginTop={'$2'}>
+								<XStack
+									alignItems='center'
+									onPress={() =>
+										Linking.openURL(
+											'https://github.com/Jellify-Music/App/issues',
+										)
+									}
+								>
+									<Icon name='github' small color='$borderColor' />
+									<Text>Report Issue</Text>
+								</XStack>
+							</XStack>
+						),
+					},
+					{
+						title: 'Powered by listeners like you',
+						subTitle: 'Sponsor Jellify on GitHub or Patreon',
 						iconName: 'heart',
 						iconColor: '$primary',
 						children: (
 							<FlatList
 								data={patrons}
-								numColumns={2}
-								renderItem={({ item }) => <Text>{item.fullName}</Text>}
+								ListHeaderComponent={
+									<YStack>
+										<XStack
+											justifyContent='flex-start'
+											gap={'$4'}
+											marginVertical={'$2'}
+										>
+											<XStack
+												alignItems='center'
+												gap={'$2'}
+												onPress={() =>
+													Linking.openURL(
+														'https://github.com/sponsors/anultravioletaurora/',
+													)
+												}
+											>
+												<Icon name='github' small color='$borderColor' />
+												<Text>Sponsors</Text>
+											</XStack>
+											<XStack
+												alignItems='center'
+												gap={'$2'}
+												onPress={() =>
+													Linking.openURL(
+														'https://patreon.com/anultravioletaurora',
+													)
+												}
+											>
+												<Icon name='patreon' small color='$borderColor' />
+												<Text>Patreon</Text>
+											</XStack>
+										</XStack>
+
+										<Separator marginBottom={'$3'} />
+
+										<Text fontSize={'$5'}>Patreon Wall of Fame</Text>
+									</YStack>
+								}
+								numColumns={1}
+								renderItem={({ item }) => (
+									<XStack alignItems='flex-start' maxWidth={'$20'}>
+										<Text numberOfLines={1} lineBreakStrategyIOS='standard'>
+											{item.fullName}
+										</Text>
+									</XStack>
+								)}
 							/>
 						),
 					},
