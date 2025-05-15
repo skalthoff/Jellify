35359c2538d199467847d709ecf034d010b1d1fc
Violet Caulfield <violet@cosmonautical.cloud>
2025-05-15 11:26:32 -0500

    update workflow

--- scripts/ota-iOS.sh
+++ scripts/ota-iOS.sh
@@ -8,8 +8,6 @@ cd ../..
 yarn createBundle:ios
 cd ios/App-Bundles
 git add .
-git config --global user.email "violet@cosmonautical.cloud"
-git config --global user.name "anultravioletaurora"
 git commit -m "OTA-Update - $(date +'%b %d %H:%M')"
 git push origin head
 cd ..
